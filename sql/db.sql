f9854142c56aeaf262d74ded42b0e42c8f61425f
hellow10 <39315875+hellow10@users.noreply.github.com>
2018-10-26 14:32:16 -0300

    Add files via upload

---
+++ sql/db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE tptickets;
+use tptickets;
+
+CREATE TABLE artists(
+id_artist int AUTO_INCREMENT PRIMARY KEY,
+name varchar(40)
+);
+
+INSERT INTO artists(name) 
+VALUES
+("Metallica"),
+("Red Hot Chili Peppers"),
+("Bob Dylan"),
+("Iron Maiden"),
+("ZZ Top"),
+("Rata Blanca");
