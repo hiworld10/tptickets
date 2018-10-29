40e244309cba476852545c844287c46b581115ce
hellow10 <39315875+hellow10@users.noreply.github.com>
2018-10-29 00:22:31 -0300

    delete comment blocks, indentation in router

--- config/router.php
+++ config/router.php
@@ -1,63 +1,34 @@
 <?php 
 namespace config;
 
-    class Router {
+class Router {
 
-        /**
-         * Se encarga de direccionar a la pagina solicitada
-         *
-         * @param Request
-         */
+    public function __construct()
+    {
+    }
 
-        public function __construct()
-        {
-            # code...
-        }
+    public static function direct(Request $request) {
 
-        public static function direct(Request $request) {
+        $controller = $request->getController() . 'Controller';
 
-            /**
-             *
-             */
-            $controller = $request->getController() . 'Controller';
+        $method = $request->getMethod();
 
-            /**
-             *
-             */
-            $method = $request->getMethod();
+        $parameters = $request->getParameters();
 
-            /**
-             *
-             */
-            $parameters = $request->getParameters();
+        $class = "controllers\\". $controller;
 
-            /**
-             *
-             */
-            $class = "controllers\\". $controller;
+        $instance = new $class;
 
-            /**
-             *
-             */
-            $instance = new $class;
+        if($_FILES) {
+           $parameters[] = $_FILES;
+       }
 
-
-            /**
-             *
-             */
-            if($_FILES) {
-                 $parameters[] = $_FILES;
-            }
-
-            /**
-             *
-             */
-            if(!isset($parameters)) {
-                call_user_func(array($instance, $method));
-            } else {
-                call_user_func_array(array($instance, $method), $parameters);
-            }
+        if(!isset($parameters)) {
+            call_user_func(array($instance, $method));
+        } else {
+            call_user_func_array(array($instance, $method), $parameters);
         }
     }
+}
 
 ?>
