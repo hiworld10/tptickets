bb5b4a49ff067eb4f836d3fc065b89991ee58095
frachy97 <38957593+frachy97@users.noreply.github.com>
2018-10-30 13:32:13 -0300

    Add files via upload

--- dao/lists/calendardao.php
+++ dao/lists/calendardao.php
@@ -1,75 +1,72 @@
 <?php 
 namespace dao\lists;
-
-
+use dao\IDAO as IDAO;
 class CalendarDAO implements IDAO
 {
 	private $list;
-
 	function __construct() {
 		if (!isset($_SESSION)) {
 			
 			session_start();
 		}
-		if (isset($_SESSION["calendar"])) {
-			$this->list = $_SESSION["calendar"];
+		if (isset($_SESSION["calendars"])) {
+			$this->list = $_SESSION["calendars"];
 		} 
 		else {
 			$this->list = array();
+			$_SESSION["idcalendars"] = 0;
 		}
-
 	}
-
 	public function create($newVal) {
-		if (isset($_SESSION["calendar"])) {
-			$this->list = $_SESSION["calendar"];
+		if (isset($_SESSION["calendars"])) {
+			$this->list = $_SESSION["calendars"];
 		} 
-
+		$newVal->setId(++$_SESSION["idcalendars"]);
 		array_push($this->list, $newVal);
-
-		$_SESSION["calendar"] = $this->list;
-
+		$_SESSION["calendars"] = $this->list;
 	}
-
-	public function getByDate($date) {
-		if (isset($_SESSION["calendar"])) {
-			$this->list = $_SESSION["calendar"];
-
+	public function retrieveById($id) {
+		if (isset($_SESSION["calendars"])) {
+			$this->list = $_SESSION["calendars"];
 			foreach ($this->list as $key => $value) {
-				if ($id == $value->getDate()) {
+				if ($id == $value->getId()) {
 					return $value;
 				}
 			}
 		}
-
 		return false;
 	}
-
-	public function retrieveById($id) {
-		
-	}
-
 	public function retrieveAll() {
-		if(isset($_SESSION['calendar'])) {
-			$this->list = $_SESSION['calendar'];
+		if(isset($_SESSION['calendars'])) {
+			$this->list = $_SESSION['calendars'];
 			return $this->list;
-
 		}
 		return false;
 	}
 	
-
-
-public function update($newVal) {
-
-}
-
-public function delete($newVal) {
-
-}
-
-
-
-
+	public function update($newVal) {
+		if(isset($_SESSION['calendars'])) {
+			$this->list = $_SESSION['calendars'];
+			foreach ($this->list as $key => $value) {
+				if ($newVal->getId() == $value->getId()) {
+					$value->setDate($newVal->getDate());
+					$value->setEventId($newVal->getEventId());
+				}
+			}
+		}
+		$_SESSION['calendars'] = $this->list;
+	}
+	public function delete($newVal) {
+		if(isset($_SESSION['calendars'])) {
+			
+			$list= $_SESSION['calendars'];
+			foreach ($list as $key => $value) {
+				if($newVal == $value->getId()) {
+					unset($list[$key]);	
+				}
+				$_SESSION['calendars']= $list;
+			}
+		}
+	}
 }
 ?>
