d4453f7c4d69c7b446785c1cb39ddbf6f9ea8c1f
frachy97 <38957593+frachy97@users.noreply.github.com>
2018-10-30 13:30:54 -0300

    Add files via upload

--- controllers/calendarcontroller.php
+++ controllers/calendarcontroller.php
@@ -1,33 +1,54 @@
 <?php 
 namespace controllers;
-
 use model\Calendar as M_Calendar;
-use dao\lists\CalendarDAO as CalendarDAO;
+use dao\lists\CalendarDAO as List_CalendarDAO;
+use dao\db\CalendarDAO as DB_CalendarDAO;
+use controllers\EventController as EventController;
 
-
-class ArtistController {
+class CalendarController {
 
 	private $dao;
+	private $eventController;
 
 	public function __construct() {
-		$this->dao = new CalendarDAO();
+		$this->dao = new List_CalendarDAO();
+		$this->eventController = new EventController();
 	}
 
-	public function addCalendar($date) {
-
-		$m_calendar = new M_Calendar($date);
+	public function addCalendar($date, $eventId) {
+		$m_calendar = new M_Calendar(null, $date, $eventId);
 		$this->dao->create($m_calendar);
 		$this->getAll();
-
-
 	}
 
-	public function getAll(){
-		$calendarArray = $this->dao->retrieve(); 
-		include ROOT . '/views/home.php';
+	public function getCalendar($id) { 
+		$calendar=$this->dao->retrieveById($id);
+		$eventArray = $this->eventController->getAllSelect();	
+		if(isset($calendar) && isset($eventArray)) {
+			include ADMIN_VIEWS . '/admincalendar.php';
+		}
 	}
 
+	public function getAll() {
+		$calendarArray = $this->dao->retrieveAll();
+		$eventArray = $this->eventController->getAllSelect();
+		include ADMIN_VIEWS. '/admincalendar.php';
+	}
 
+	public function deleteCalendar($id){
+		$this->dao->delete($id);
+		$this->getAll();
+	}
+
+	public function updateCalendar($id, $date, $newEventId) {
+		$updatedCalendar = new M_Calendar($id, $date, $newEventId);
+		$this->dao->update($updatedCalendar);
+		$this->getAll();
+	}
+
+	public function getAllSelect(){
+		return $this->dao->retrieveAll();
+	}
+	
 }
-
 ?>
