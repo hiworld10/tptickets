09d1b77aa73565dd132ef5ac206a2559ea7bb251
hellow10 <39315875+hellow10@users.noreply.github.com>
2018-10-30 13:49:43 -0300

    Add href to Calendar entry

--- views/menuadmin.php
+++ views/menuadmin.php
@@ -3,7 +3,7 @@
   <li><a href="<?=FRONT_ROOT?>/artist/getAll" >Artistas</a></li>
  <li><a href="<?=FRONT_ROOT?>/category/getAll"  title="...">Categorias</a></li>
   <li><a href="<?=FRONT_ROOT?>/event/getAll" title="...">Eventos</a></li>
-  <li><a href="..." title="...">Calendario</a></li>
+  <li><a href="<?=FRONT_ROOT?>/calendar/getAll" title="...">Calendario</a></li>
   <li><a href="<?=FRONT_ROOT?>/user/getAll" title="...">Usuarios</a></li>
   <li><a href="..." title="...">Plazas</a></li>
    <li><a href="<?=FRONT_ROOT?>/seattype/getAll" title="...">Tipo Plazas</a></li>
