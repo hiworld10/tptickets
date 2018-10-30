0e541795785ffb41d462d206f56f7d68e93dc041
frachy97 <38957593+frachy97@users.noreply.github.com>
2018-10-30 13:30:23 -0300

    Add files via upload

--- model/calendar.php
+++ model/calendar.php
@@ -2,24 +2,31 @@
 namespace model;
 
 
+
 class Calendar
 {
-    private $id
+    private $id;
 	private $date;
-	private $m_event;
-	private $m_eventSeat;
-    private $m_artist;
-    private $m_eventVenue;
+    private $eventId;
 
-	function __construct($id, $date, $m_event, $m_eventSeat, $m_artist, $m_eventVenue)
+	function __construct($id, $date, $eventId)
 	{
         $this->id= $id;
         $this->date= $date;
-        $this->m_event= $m_event;
-        $this->m_eventSeat= $m_eventSeat;
-        $this->m_artist= $m_artist;
-        $this->m_eventVenue= $m_eventVenue;
+        $this->eventId= $eventId;
 	}
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
 
     public function getId()
     {
@@ -31,54 +38,14 @@ class Calendar
         $this->id = $id;
     }
 
-    public function getDate()
+    public function getEventId()
     {
-        return $this->date;
+        return $this->eventId;
     }
 
-    public function setDate($date)
+    public function setEventId($eventId)
     {
-        $this->date = $date;
-    }
-
-    public function getMEvent()
-    {
-        return $this->m_event;
-    }
-
-    public function setMEvent($m_event)
-    {
-        $this->m_event = $m_event;
-    }
-
-    public function getMEventSeat()
-    {
-        return $this->m_eventSeat;
-    }
-
-    public function setMEventSeat($m_eventSeat)
-    {
-        $this->m_eventSeat = $m_eventSeat;
-    }
-
-    public function getMArtist()
-    {
-        return $this->m_artist;
-    }
-
-    public function setMArtist($m_artist)
-    {
-        $this->m_artist = $m_artist;
-    }
-
-    public function getMEventVenue()
-    {
-        return $this->m_eventVenue;
-    }
-
-    public function setMEventVenue($m_eventVenue)
-    {
-        $this->m_eventVenue = $m_eventVenue;
+        $this->eventId = $eventId;
     }
 }
 ?>
