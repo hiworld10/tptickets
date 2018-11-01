0a05ec013d9b60d69090859bbd8e668f1e8b87aa
frachy97 <38957593+frachy97@users.noreply.github.com>
2018-10-31 21:39:44 -0300

    singin

--- views/singin.php
+++ views/singin.php
@@ -1,43 +1,62 @@
 <!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Registrarse</title>
-</head>
-<body>
-    <form id="registrarse-form" class="m-2" action="../acciones/registrarse.php" method="POST">
-            <div class="text-center mb-4">
-                <strong>LOGIN</strong>
-            </div>
+<html lang="en">
 
-            <?php if(isset($alert)) { ?>
-                <div class="alert alert-danger text-center">
-                    <?= $alert ?>
-                </div>
-            <?php } ?>
+<?php include_once(VIEWS_ROOT.HEADER); ?>
 
-            <fieldset>
-                <div class="form-group">
-                    <label for="">E-mail</label>
-                    <input type="email" name="email" value="mi@email.com" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="">Contraseña</label>
-                    <input type="password" name="pass" value="" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="">Nombre</label>
-                    <input type="nombre" name="nombre" value="" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="">Apellido</label>
-                    <input type="apellido" name="apellido" value="" class="form-control">
-                </div>
-                
-            </fieldset>
-            <button type="submit" class="btn btn-primary btn-block">Registrarse</button>
-        </form>
+  <body>
 
-</body>
+ <?php include_once(VIEWS_ROOT.NAVBAR); ?>
+
+<style type="text/css">
+  main{
+    background-image: url("<?= IMG_FRONT_ROOT?>/fondologin.jpg");
+    height: auto;
+    width: auto;
+  }
+  h2{
+    color: white;
+  }
+</style>
+
+  <main class="d-flex align-items-center justify-content-center height-100">
+          <div class="content">
+               <header class="text-center mt-5">
+                    <h2>Registrarse!</h2>
+                   
+               </header>
+               <form class="login-form bg-dark-alpha p-5 text-white" action="<?=FRONT_ROOT?>/home/addUser ?>" method="POST">
+                    <div class="form-group">
+                         <label for="">Email</label>
+                         <input type="email" name="email" class="form-control form-control-lg" placeholder="Ingresar email">
+                    </div>
+                    <div class="form-group">
+                         <label for="">Contraseña</label>
+                         <input type="text" name="password" class="form-control form-control-lg" placeholder="Ingresar contraseña">
+                    </div>
+                    <div class="form-group">
+                         <label for="">Nombre</label>
+                         <input type="text" name="name" class="form-control form-control-lg" placeholder="Ingresar nombre">
+                    </div>
+                     <div class="form-group">
+                         <label for="">Apellido</label>
+                         <input type="text" name="lastname" class="form-control form-control-lg" placeholder="Ingresar apellido">
+                    </div>
+                    <button class="btn btn-dark btn-block btn-lg mt-5" type="submit">
+                         Registrarse
+                    </button>
+               </form>
+          </div>
+     </main>
+
+    <?php include_once(VIEWS_ROOT.FOOTER); ?>
+
+   
+    
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
 </html>
-
