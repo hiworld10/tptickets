3baa41f3a0aea04b1798d91f9db673bb493f6471
hellow10 <39315875+hellow10@users.noreply.github.com>
2018-11-01 23:51:22 -0300

    Indentation

--- views/admin/adminartist.php
+++ views/admin/adminartist.php
@@ -1,113 +1,113 @@
 
-  <!DOCTYPE html>
+<!DOCTYPE html>
 
 
-  <html lang="en" class="__full-height-perc">
+<html lang="en" class="__full-height-perc">
 
-     <?php include_once(VIEWS_ROOT.HEADER); ?>
+<?php include_once(VIEWS_ROOT.HEADER); ?>
 
-    <body class="__full-height-perc">
+<body class="__full-height-perc">
 
-        <?php include_once(VIEWS_ROOT.NAVBAR); ?>
+  <?php include_once(VIEWS_ROOT.NAVBAR); ?>
 
- 
+  
   
 
   <div id="container" class="__full-height-perc">
 
-       <?php include_once(VIEWS_ROOT.MENUADMIN); ?>
+   <?php include_once(VIEWS_ROOT.MENUADMIN); ?>
 
 
-    <div id="divform"  class="__full-height-perc">
+   <div id="divform"  class="__full-height-perc">
 
     <div id=edit>
       <!-- Este div aparecerá si un artista debe ser modificado -->
       <?php if(isset($artist)) { ?>
 
         <form name='formulario' action="<?=FRONT_ROOT?>/artist/updateArtist" method="POST">
-             <div class="form-row">
-                  <div class="col-md-1 mb-2 mb-md-0 form-row">
-                    <label> Id</label>
-                    <input type="text" name="id" class="form-control form-control-lg" value="<?= $artist->getId(); ?>" readonly>
-                  </div>
-                  <div class="col-md-7 mb-2 mb-md-0 form-row">
-                    <label> Nombre</label>
-                    <input type="text" name="artist" class="form-control form-control-lg" value="<?= $artist->getName(); ?>" required>
-                  </div>
-                  <div class="col-12 col-md-2 mt-4">
-                    <button type="submit"  class="btn btn-lg btn-primary">Aceptar</button>
-                  </div>
-           </div>
-        </form>
+         <div class="form-row">
+          <div class="col-md-1 mb-2 mb-md-0 form-row">
+            <label> Id</label>
+            <input type="text" name="id" class="form-control form-control-lg" value="<?= $artist->getId(); ?>" readonly>
+          </div>
+          <div class="col-md-7 mb-2 mb-md-0 form-row">
+            <label> Nombre</label>
+            <input type="text" name="artist" class="form-control form-control-lg" value="<?= $artist->getName(); ?>" required>
+          </div>
+          <div class="col-12 col-md-2 mt-4">
+            <button type="submit"  class="btn btn-lg btn-primary">Aceptar</button>
+          </div>
+        </div>
+      </form>
 
     <?php   } else {   ?>
 
-        <!-- De no ser así, se habilitará el formulario para agregar artistas  -->
-        <form name='formulario' action="<?=FRONT_ROOT?>/artist/addArtist"  method="POST">
-             <div class="form-row">
-                  <div class="col-12 col-md-9 mb-2 mb-md-0">
-                    <input type="text" name="artist" class="form-control form-control-lg" placeholder="Ingrese nombre del artista..." required>
-                  </div>
-                  <div class="col-12 col-md-3">
-                    <button type="submit" class="btn btn-block btn-lg btn-primary">Agregar</button>
-                  </div>
-                </div>
+      <!-- De no ser así, se habilitará el formulario para agregar artistas  -->
+      <form name='formulario' action="<?=FRONT_ROOT?>/artist/addArtist"  method="POST">
+       <div class="form-row">
+        <div class="col-12 col-md-9 mb-2 mb-md-0">
+          <input type="text" name="artist" class="form-control form-control-lg" placeholder="Ingrese nombre del artista..." required>
+        </div>
+        <div class="col-12 col-md-3">
+          <button type="submit" class="btn btn-block btn-lg btn-primary">Agregar</button>
+        </div>
+      </div>
     </form>
 
   <?php } ?>
 
-    </div>
+</div>
 
 <!-- Lista de artistas -->
-  <div id="table">
-    <table class="table bg-light-alpha">
+<div id="table">
+  <table class="table bg-light-alpha">
 
-          <?php if(!empty($artistArray)) { ?>
-              <thead>     
-                 <th>Id</th>  
-                 <th>Nombre</th>            
-             </thead>
-             <tbody>
-              <?php foreach ($artistArray as $value) { ?>
-                  <tr>
-                     <td><?= $value->getId(); ?></td>
-                      <td><?= $value->getName(); ?></td>
-                      <td>
-                        <form action="<?=FRONT_ROOT?>/artist/deleteArtist" method="POST">
-                        <button name="iddelete" value="<?= $value->getId();  ?>" id="boton1" type="submit"class="btn btn-block btn-lg btn-primary btn-sm">Eliminar</button>
-                        </form>
-                      <td>
-                        <form action="<?=FRONT_ROOT?>/artist/getArtist" method="POST">
-                        <button name="update" value="<?= $value->getId(); ?>" id="boton1" type="submit" class="btn btn-block btn-lg btn-primary btn-sm">Editar</button></td>
-                        </form>
-                  </tr>
-              <?php } ?>
+    <?php if(!empty($artistArray)) { ?>
+      <thead>     
+       <th>Id</th>  
+       <th>Nombre</th>            
+     </thead>
+     <tbody>
+      <?php foreach ($artistArray as $value) { ?>
+        <tr>
+         <td><?= $value->getId(); ?></td>
+         <td><?= $value->getName(); ?></td>
+         <td>
+          <form action="<?=FRONT_ROOT?>/artist/deleteArtist" method="POST">
+            <button name="iddelete" value="<?= $value->getId();  ?>" id="boton1" type="submit"class="btn btn-block btn-lg btn-primary btn-sm">Eliminar</button>
+          </form>
+          <td>
+            <form action="<?=FRONT_ROOT?>/artist/getArtist" method="POST">
+              <button name="update" value="<?= $value->getId(); ?>" id="boton1" type="submit" class="btn btn-block btn-lg btn-primary btn-sm">Editar</button></td>
+            </form>
+          </tr>
+        <?php } ?>
 
-          </tbody>
-      <?php  } ?>
+      </tbody>
+    <?php  } ?>
   </table>
-
-  </div>
 
 </div>
 
-    </div>
+</div>
 
-  </div>
+</div>
 
-
-
-   
+</div>
 
 
-      
 
-     
-    
 
-      <!-- Bootstrap core JavaScript -->
-      <script src="vendor/jquery/jquery.min.js"></script>
-      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    </body>
 
-  </html>
+
+
+
+
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
--- views/admin/admincalendar.php
+++ views/admin/admincalendar.php
@@ -2,149 +2,149 @@
 <!-- NO TERMINADO SOLO COPIADO Y PEGADO OJO -->
 
 
-  <html lang="en" class="__full-height-perc">
+<html lang="en" class="__full-height-perc">
 
-    <?php include_once(VIEWS_ROOT.HEADER); ?>
+<?php include_once(VIEWS_ROOT.HEADER); ?>
 
-    <body class="__full-height-perc">
+<body class="__full-height-perc">
 
-       <?php include_once(VIEWS_ROOT.NAVBAR); ?>
+ <?php include_once(VIEWS_ROOT.NAVBAR); ?>
 
 
  
 
-  <div id="container" class="__full-height-perc">
-      <?php include_once(VIEWS_ROOT.MENUADMIN); ?>
+ <div id="container" class="__full-height-perc">
+  <?php include_once(VIEWS_ROOT.MENUADMIN); ?>
 
 
 
-    <div id="divform" class="__full-height-perc">
+  <div id="divform" class="__full-height-perc">
 
-      <?php if(isset($calendar)){ ?>
-
-
-        <form name='formulario' action="<?=FRONT_ROOT?>/calendar/updateCalendar"  method="POST">
-             <div class="form-row">
-                  <div class="col-md-1 mb-2 mb-md-0 form-row">
-                    <label>Id</label>
-                    <input type="text" name="id" class="form-control form-control-lg" value="<?= $calendar->getId(); ?>" readonly>
-                  </div>
-                  <div class="col-md-5 mb-2 mb-md-0 form-row">
-                    <label>Fecha</label>
-                    <input type="date" name="date" class="form-control form-control-lg" value="<?= $calendar->getDate(); ?>" required>
-                  </div>
-                  <div class="col-md-5 mb-2 mb-md-0 form-row">
-                    <label>Fecha</label>
-                         <select class="form-control" name="event" required>
-                                        <?php if($eventArray){ 
-                                             foreach ($eventArray as $key => $value) { 
-                                         ?>
-                                        <option value="<?= $value->getId(); ?>"><?= $value->getName(); ?></option> 
-                                        <?php }
-                                         }else{ ?>
-                                        <option >NO HAY EVENTOS</option>
-                                   <?php } ?>
-                          </select>
-                  </div>
-                  <div class="col-12 col-md-2 form-row">
-                    <button type="submit" class="btn btn-block btn-lg btn-primary">Aceptar</button>
-                  </div>
-                </div>
-    </form>
-
-    <?php   }else{  ?>
-
-    <form name='formulario' action="<?=FRONT_ROOT?>/calendar/addCalendar"  method="POST">
-             <div class="form-row p-2">
-                  <div class="form-group col-12 col-md-9 mb-2 mb-md-0">
-                    <input type="date"   class="form-control form-control-lg" name="date" placeholder="Ingrese fecha..." required>
-                  </div>
-                  <div class="col-lg-4 form-group">
-                        
-                                   <label for="">Evento</label>
-                                   <select class="form-control" name="event" required>
-                                        <?php if($eventArray){ 
-                                             foreach ($eventArray as $key => $value) { 
-                                         ?>
-                                        <option value="<?= $value->getId(); ?>"><?= $value->getName(); ?></option> 
-                                        <?php }
-                                         }else{ ?>
-                                        <option >NO HAY EVENTOS</option>
-                                   <?php } ?>
-                                   </select>
-                        
-                      </div>
-                    <?php if($eventArray) { ?>
-                  <div class="col-11 col-md-3 mt-4">
-                    <button type="submit" class="btn btn-block btn-lg btn-primary">Agregar</button>
-                  </div>
-                <?php }else{ ?>
-                  <div class="col-11 col-md-3 mt-4">
-                    <button type="submit" disabled class="btn btn-block btn-lg btn-primary">Agregar</button>
-                  </div>
-                <?php } ?>
-
-           </div>
-   </form>
+    <?php if(isset($calendar)){ ?>
 
 
-  <?php } ?>
+      <form name='formulario' action="<?=FRONT_ROOT?>/calendar/updateCalendar"  method="POST">
+       <div class="form-row">
+        <div class="col-md-1 mb-2 mb-md-0 form-row">
+          <label>Id</label>
+          <input type="text" name="id" class="form-control form-control-lg" value="<?= $calendar->getId(); ?>" readonly>
+        </div>
+        <div class="col-md-5 mb-2 mb-md-0 form-row">
+          <label>Fecha</label>
+          <input type="date" name="date" class="form-control form-control-lg" value="<?= $calendar->getDate(); ?>" required>
+        </div>
+        <div class="col-md-5 mb-2 mb-md-0 form-row">
+          <label>Fecha</label>
+          <select class="form-control" name="event" required>
+            <?php if($eventArray){ 
+             foreach ($eventArray as $key => $value) { 
+               ?>
+               <option value="<?= $value->getId(); ?>"><?= $value->getName(); ?></option> 
+             <?php }
+           }else{ ?>
+            <option >NO HAY EVENTOS</option>
+          <?php } ?>
+        </select>
+      </div>
+      <div class="col-12 col-md-2 form-row">
+        <button type="submit" class="btn btn-block btn-lg btn-primary">Aceptar</button>
+      </div>
+    </div>
+  </form>
 
-  <div id="table" class="__full-height-perc">
-    <table class="table bg-light-alpha">
+<?php   }else{  ?>
 
-          <?php if(!empty($calendarArray)) { ?>
-              <thead>     
-                 <th>Id</th>  
-                 <th>Fecha</th>     
-                 <th>Evento</th>          
-             </thead>
-             <tbody>
-              <?php foreach ($calendarArray as $value) { ?>
-                  <tr>
-                     <td><?= $value->getId(); ?></td>
-                      <td><?= $value->getDate(); ?></td>
-                       <td><?= $value->getEventId(); ?></td>
-
-                      <td>
-                        <form action="<?=FRONT_ROOT?>/calendar/deleteCalendar" method="POST">
-                        <button name="iddelete" value="<?= $value->getId();  ?>"id="boton1" type="submit"class="btn btn-block btn-lg btn-primary btn-sm">Eliminar</button>
-                        </form>
-                      </td>
-                      <td>
-                        <form action="<?=FRONT_ROOT?>/calendar/getCalendar" method="POST">
-                        <button name="update" value="<?= $value->getId(); ?>" id="boton1" type="submit" class="btn btn-block btn-lg btn-primary btn-sm">Editar</button></td>
-                        </form>
-                      </td>
-                  </tr>
-              <?php } ?>
-
-          </tbody>
-      <?php  } ?>
-  </table>
-
+  <form name='formulario' action="<?=FRONT_ROOT?>/calendar/addCalendar"  method="POST">
+   <div class="form-row p-2">
+    <div class="form-group col-12 col-md-9 mb-2 mb-md-0">
+      <input type="date"   class="form-control form-control-lg" name="date" placeholder="Ingrese fecha..." required>
+    </div>
+    <div class="col-lg-4 form-group">
+      
+     <label for="">Evento</label>
+     <select class="form-control" name="event" required>
+      <?php if($eventArray){ 
+       foreach ($eventArray as $key => $value) { 
+         ?>
+         <option value="<?= $value->getId(); ?>"><?= $value->getName(); ?></option> 
+       <?php }
+     }else{ ?>
+      <option >NO HAY EVENTOS</option>
+    <?php } ?>
+  </select>
+  
+</div>
+<?php if($eventArray) { ?>
+  <div class="col-11 col-md-3 mt-4">
+    <button type="submit" class="btn btn-block btn-lg btn-primary">Agregar</button>
   </div>
+<?php }else{ ?>
+  <div class="col-11 col-md-3 mt-4">
+    <button type="submit" disabled class="btn btn-block btn-lg btn-primary">Agregar</button>
+  </div>
+<?php } ?>
+
+</div>
+</form>
+
+
+<?php } ?>
+
+<div id="table" class="__full-height-perc">
+  <table class="table bg-light-alpha">
+
+    <?php if(!empty($calendarArray)) { ?>
+      <thead>     
+       <th>Id</th>  
+       <th>Fecha</th>     
+       <th>Evento</th>          
+     </thead>
+     <tbody>
+      <?php foreach ($calendarArray as $value) { ?>
+        <tr>
+         <td><?= $value->getId(); ?></td>
+         <td><?= $value->getDate(); ?></td>
+         <td><?= $value->getEventId(); ?></td>
+
+         <td>
+          <form action="<?=FRONT_ROOT?>/calendar/deleteCalendar" method="POST">
+            <button name="iddelete" value="<?= $value->getId();  ?>"id="boton1" type="submit"class="btn btn-block btn-lg btn-primary btn-sm">Eliminar</button>
+          </form>
+        </td>
+        <td>
+          <form action="<?=FRONT_ROOT?>/calendar/getCalendar" method="POST">
+            <button name="update" value="<?= $value->getId(); ?>" id="boton1" type="submit" class="btn btn-block btn-lg btn-primary btn-sm">Editar</button></td>
+          </form>
+        </td>
+      </tr>
+    <?php } ?>
+
+  </tbody>
+<?php  } ?>
+</table>
 
 </div>
 
-    </div>
+</div>
 
-  </div>
+</div>
 
-
-
-   
+</div>
 
 
-      
 
-     
-    
 
-      <!-- Bootstrap core JavaScript -->
-      <script src="vendor/jquery/jquery.min.js"></script>
-      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    </body>
 
-  </html>
+
+
+
+
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
