6e116de3e5098674708f9db8ecd33950d7410a28
frachy97 <38957593+frachy97@users.noreply.github.com>
2018-11-05 13:42:04 -0300

    update

--- views/menuadmin.php
+++ views/menuadmin.php
@@ -7,5 +7,8 @@
   <li><a href="<?=FRONT_ROOT?>/user/getAll" title="...">Usuarios</a></li>
   <li><a href="..." title="...">Plazas</a></li>
    <li><a href="<?=FRONT_ROOT?>/seattype/getAll" title="...">Tipo Plazas</a></li>
+   <li><a href="<?=FRONT_ROOT?>/placeevent/getAll" title="...">Lugar</a></li>
+   <li><a href="<?=FRONT_ROOT?>/purchase/getAll" title="...">Compra</a></li>
+   <li><a href="<?=FRONT_ROOT?>/purchaseline/getAll" title="...">Linea Compra</a></li>
  </ul>
 </div>
