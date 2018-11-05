a5ecde6a4dede82815c9b3367398c8d4a61875fd
hellow10 <39315875+hellow10@users.noreply.github.com>
2018-11-05 13:39:42 -0300

    Change var names

--- model/placeevent.php
+++ model/placeevent.php
@@ -4,14 +4,14 @@ namespace model;
 class PlaceEvent
 {
 	private $id;
-	private $id_calendar;
+	private $idCalendar;
 	private $capacity;
 	private $description;
 
-	function __construct($id, $id_calendar, $capacity, $description)
+	function __construct($id, $idCalendar, $capacity, $description)
 	{
 		$this->id = $id;
-		$this->id_calendar = $id_calendar;
+		$this->idCalendar = $idCalendar;
 		$this->capacity = $capacity;
 		$this->description = $description;
 	}
@@ -24,7 +24,7 @@ class PlaceEvent
 
 	public function getCalendarId()
 	{
-		return $this->id_calendar;
+		return $this->idCalendar;
 	}
 
 	public function getCapacity()
@@ -40,23 +40,23 @@ class PlaceEvent
 	
 	public function setId($id)
 	{
-		$this->id=$id;
+		$this->id = $id;
 	}
 
 
-	public function setIdCalendar($id_calendar)
+	public function setIdCalendar($idCalendar)
 	{
-		$this->id_calendar=$id_calendar;
+		$this->idCalendar = $idCalendar;
 	}
 
 	public function setCapacity($capacity)
 	{
-		$this->capacity=$capacity;
+		$this->capacity = $capacity;
 	}
 
 	public function setDescription($description)
 	{
-		$this->description=$description;
+		$this->description = $description;
 	}
 
 
