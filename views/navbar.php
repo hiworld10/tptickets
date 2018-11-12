dfe742dab1793bd8176d95afb1087914b2161b5f
frachy97 <38957593+frachy97@users.noreply.github.com>
2018-11-11 21:43:56 -0300

    session working

--- views/navbar.php
+++ views/navbar.php
@@ -2,17 +2,26 @@
 
 <!-- Navigation -->
 
+<?php 
 
 
+ if(!isset($_SESSION)) 
+    session_start();
+
+
+ ?>
     <nav class="navbar navbar-light bg-light static-top">
       <div class="container">
         <a class="navbar-brand" href="<?= FRONT_ROOT ?>/home/">IziTicket</a>
         <div class="right">
-        	<?php if(isset($_SESSION['userLogedIn'])){ ?>
-            <?php $_SESSION['userLogedIn']=$user; ?>
+        	<?php if(isset($_SESSION['userLogedIn']) ){ 
+            $user= $_SESSION['userLogedIn'];
+           
+            ?>
 
 
-        	<a class="navbar-brand" href="..."><?= $user->getFirstname(); ?></a>
+
+        	
           	<a class="btn btn-primary" href="<?= FRONT_ROOT ?>/user/logout">Log out</a>
           <?php }else{ ?>
           	<a class="btn btn-primary" href="<?= FRONT_ROOT ?>/user">Ingresar</a>
