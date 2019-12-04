8827ed01172c5a3b343ef9b7c3b65bcbd313dbe0
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2019-12-04 01:18:28 -0300

    Change autoload function to only strtolower the class name variable, not entire path

--- config/autoload.php
+++ config/autoload.php
@@ -6,8 +6,8 @@ class Autoload {
 	public static function start(){
 		spl_autoload_register(function($className){
 
+            $className = strtolower($className);
 			$path = str_replace("\\", '/', ROOT.$className) . ".php";
-			$path = strtolower($path);
 			require($path);
 		});
 	}
