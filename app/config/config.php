0f84fb4d93473af84fb4a4137020550e8fc3d1df
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-24 02:53:16 -0300

    Modifications to defines in config file, Photo model working again

--- app/config/config.php
+++ app/config/config.php
@@ -1,12 +1,12 @@
 <?php
 
 /* BACK */
-define('ROOT', dirname(__DIR__).'/');
-define('VIEWS', ROOT . '/views');
-define('ADMIN_VIEWS', ROOT . '/views/admin');
-define('IMG_UPLOADS', ROOT. '/img');
-define('VIEWS_ROOT', ROOT . '/views/');
-
+define('PROJECT_ROOT', dirname(dirname(__DIR__)));
+define('APP_ROOT', dirname(__DIR__));
+define('PUBLIC_ROOT', PROJECT_ROOT . '/public');
+define('VIEWS_ROOT', APP_ROOT . '/views/');
+define('ADMIN_VIEWS', APP_ROOT . '/views/admin');
+define('IMG_UPLOADS', PUBLIC_ROOT . '/img');
 
 /* FRONT */
 define('FRONT_ROOT', 'http://localhost/tptickets');
@@ -18,7 +18,6 @@ define('NAVBAR','navbar.php');
 define('MENUADMIN', 'menuadmin.php');
 define('FOOTER', 'footer.php');
 
-
 /* MEDIA */
 define('IMG_UPLOADS_PATH', FRONT_ROOT . '/content/uploads/img');
 define('MOV_UPLOADS_PATH', FRONT_ROOT . '/content/uploads/movies');
@@ -28,3 +27,5 @@ define('DB_HOST', 'localhost');
 define('DB_NAME', 'tptickets');
 define('DB_USER', 'root');
 define('DB_PASS', '');
+
+?>
