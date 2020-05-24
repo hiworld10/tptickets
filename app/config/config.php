02a00db927ce20b1da4d6e713e9ff8877a0d3a4c
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-24 20:36:38 -0300

    Reorganize and update defines to adjust to new views structure

--- app/config/config.php
+++ app/config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-/* BACK */
+/* Directorios principales */
 define('PROJECT_ROOT', dirname(dirname(__DIR__)));
 define('APP_ROOT', dirname(__DIR__));
 define('PUBLIC_ROOT', PROJECT_ROOT . '/public');
@@ -8,21 +8,23 @@ define('VIEWS_ROOT', APP_ROOT . '/views/');
 define('ADMIN_VIEWS', APP_ROOT . '/views/admin');
 define('IMG_UPLOADS', PUBLIC_ROOT . '/img');
 
-/* FRONT */
+/* Vistas principales */
+define('HEADER', VIEWS_ROOT . 'inc/header.php');
+define('NAVBAR', VIEWS_ROOT . 'inc/navbar.php');
+define('MENUADMIN', 'menuadmin.php');
+define('FOOTER', VIEWS_ROOT . 'inc/footer.php');
+
+/* Rutas URL */
 define('FRONT_ROOT', 'http://localhost/tptickets');
 define('ADMIN_FRONT_ROOT', FRONT_ROOT . '/views/admin');
 define('IMG_FRONT_ROOT', FRONT_ROOT . '/img');
 define('CSS_PATH', FRONT_ROOT . '/content/css');
-define('HEADER','header.php');
-define('NAVBAR','navbar.php');
-define('MENUADMIN', 'menuadmin.php');
-define('FOOTER', 'footer.php');
 
-/* MEDIA */
+/* Media */
 define('IMG_UPLOADS_PATH', FRONT_ROOT . '/content/uploads/img');
 define('MOV_UPLOADS_PATH', FRONT_ROOT . '/content/uploads/movies');
 
-/* DATABASE */
+/* Credenciales de BD */
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'tptickets');
 define('DB_USER', 'root');
