d671493256891e97a15ff6671866d94ea7ef7d2c
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-24 20:35:49 -0300

    Main views header, navbar and footer now in own folder (inc)

---
+++ app/views/inc/navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-light bg-light static-top">
+    <div class="container">
+        <a class="navbar-brand" href="<?= FRONT_ROOT ?>/home/">TPTickets</a>
+        <div class="right">
+            <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/loginScreen">Ingresar</a>
+            <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/signup">Registrarse</a>
+        </div>
+    </div>
+</nav>
