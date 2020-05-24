c664cdd7d5780c614032c8f42ed75db4877eaff6
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-24 01:20:12 -0300

    Add utils folder and StringUtils class

---
+++ app/utils/StringUtils.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace app\utils;
+/**
+ * Clase utilitaria para manipular y devolver strings.
+ */
+class StringUtils
+{
+    /**
+     * Convierte en camel case el string correspondiente a la funcion de la controladora a llamar
+     * 
+     * @param  string
+     * @return string
+     */
+    public static function convertToCamelCase($string) {
+        return lcfirst($this->convertToStudlyCaps($string));
+    }
+
+    /**
+     * Capitaliza la inicial de cada caracter posterior a un '-' del string correspondiente a la funcion de la controladora a llamar. Se utiliza en conjunto con la funcion convertToCamelCase().
+     *
+     * De este modo, en el URL se podra llamar al metodo deseado separando palabras con un '-'. Ej: 'artists/get-all'
+     * 
+     * @param  string
+     * @return string
+     */
+    public static function convertToStudlyCaps($string) {
+        return str_replace(' ', '', ucwords(str_replace('-', '', $string)));
+    }
+
+}
+
+ ?>
