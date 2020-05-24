0232829db0028d39d427e2017d9afcbf3b4bfe27
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-24 00:51:42 -0300

    Fix hrefs in admin navbar according to new structure

--- app/views/menuadmin.php
+++ app/views/menuadmin.php
@@ -1,12 +1,12 @@
 <div id="cssmenu" class="__full-height-perc">
     <ul>
-        <li><a href="<?=FRONT_ROOT?>/artist" >Artistas</a></li>
-        <li><a href="<?=FRONT_ROOT?>/category"  title="...">Categorias</a></li>
-        <li><a href="<?=FRONT_ROOT?>/event" title="...">Eventos</a></li>
-        <li><a href="<?=FRONT_ROOT?>/calendar" title="...">Calendario</a></li>
-        <li><a href="<?=FRONT_ROOT?>/eventseat" title="...">Plazas</a></li>
-        <li><a href="<?=FRONT_ROOT?>/seattype" title="...">Tipo Plazas</a></li>
-        <li><a href="<?=FRONT_ROOT?>/placeevent" title="...">Lugar</a></li>
-        <li><a href="<?=FRONT_ROOT?>/user" title="...">Usuarios</a></li>
+        <li><a href="<?=FRONT_ROOT?>/artists" >Artistas</a></li>
+        <li><a href="<?=FRONT_ROOT?>/categories"  title="...">Categorias</a></li>
+        <li><a href="<?=FRONT_ROOT?>/events" title="...">Eventos</a></li>
+        <li><a href="<?=FRONT_ROOT?>/calendars" title="...">Calendario</a></li>
+        <li><a href="<?=FRONT_ROOT?>/event-seats" title="...">Plazas</a></li>
+        <li><a href="<?=FRONT_ROOT?>/seat-types" title="...">Tipo Plazas</a></li>
+        <li><a href="<?=FRONT_ROOT?>/place-events" title="...">Lugar</a></li>
+        <li><a href="<?=FRONT_ROOT?>/users" title="...">Usuarios</a></li>
     </ul>
 </div>
