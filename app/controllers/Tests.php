e1fb9eeaea7cbc13138ff7a9fca38a8fee82fa00
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-24 01:17:02 -0300

    Add tests controller, made for general testing

--- app/controllers/Tests.php
+++ app/controllers/Tests.php
@@ -10,6 +10,16 @@ class Tests {
     public function index() {
         echo "Tests/index";
     }
+
+    public function connection($dao_type) {
+        echo "<p>DAO is of type $dao_type</p>";
+        $dao_type = ucfirst($dao_type);
+        $class = "\app\dao\db\\$dao_type" . "DAO";
+        $dao = new $class;
+        echo '<pre>';
+         print_r($dao->retrieveAll());
+         echo '</pre>'; 
+    }
 }
 
  ?>
