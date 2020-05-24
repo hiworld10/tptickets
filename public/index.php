be8d88d000b5df8e12ae77032e9f5e81b675bb9d
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-24 01:56:08 -0300

    Indentation in index.php

--- public/index.php
+++ public/index.php
@@ -1,32 +1,26 @@
 <?php
 
-  /**
-   * Mostrar errores de PHP
-   */
-  //ini_set('memory_limit', '4096M');
-  
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-  
+/**
+* Mostrar errores de PHP
+*/
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+/**
+* Archivos necesarios de inicio
+*/
+require "../app/autoload/Autoload.php";
+require "../app/config/config.php";
+/**
+* Alias
+*/
+use app\autoload\Autoload;
+use core\Router;
+use core\Request;
+/**
+* Flujo de ejecución
+*/
+Autoload::start();
+new Router(new Request);
 
-  /**
-   * Archivos necesarios de inicio
-   */
-  require "../app/autoload/Autoload.php";
-  require "../app/config/config.php";
-
-  /**
-   * Alias
-   */
-  use app\autoload\Autoload;
-  use core\Router;
-  use core\Request;
-
-  /**
-   * Flujo de ejecución
-   */
-
-  Autoload::start();
-
-  new Router(new Request);
+?>
