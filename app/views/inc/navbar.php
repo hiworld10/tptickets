316738f28d6b6d4c2472c69291bf1c24e51dac99
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-24 22:50:13 -0300

    Show different options in navbar depending if user is logged in or not

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -2,8 +2,12 @@
     <div class="container">
         <a class="navbar-brand" href="<?= FRONT_ROOT ?>/home/">TPTickets</a>
         <div class="right">
-            <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/loginScreen">Ingresar</a>
-            <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/signup">Registrarse</a>
+            <?php if(!isset($_SESSION['userLogedIn'])) : ?>
+                <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/loginScreen">Ingresar</a>
+                <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/signup">Registrarse</a>
+            <?php else :  ?>
+                <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/logout">Salir</a>
+            <?php endif; ?>  
         </div>
     </div>
 </nav>
