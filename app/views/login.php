336296a0db7a271995a8f1578f0ade6490f092ff
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-24 21:39:02 -0300

    Change text descriptions of login and signup page

--- app/views/login.php
+++ app/views/login.php
@@ -1,54 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php require HEADER; ?>
 
-<?php require(HEADER); ?>
+<main class="d-flex align-items-center justify-content-center height-100">
+    <div class="content">
+        <header class="text-center mt-5">
+            <h2>Iniciá Sesión</h2>        
+        </header>
+        <form class="login-form bg-dark-alpha p-5 text-white" action="<?=FRONT_ROOT?>/home/login" method="POST">
+            <div class="form-group">
+                <label for="">Usuario</label>
+                <input type="text" name="email" class="form-control form-control-lg" placeholder="Ingresar usuario">
+            </div>
+            <div class="form-group">
+                <label for="">Contraseña</label>
+                 <input type="password" name="password" class="form-control form-control-lg" placeholder="Ingresar contraseña">
+            </div>
+            <button class="btn btn-dark btn-block btn-lg mt-5" type="submit">
+                Ingresar
+            </button>
+        </form>
+    </div>
+</main>
 
-  <body>
+<?php require(FOOTER); ?>
 
- <?php require(NAVBAR); ?>
-
-<style type="text/css">
-  main{
-    background-image: url("<?= IMG_FRONT_ROOT?>/fondologin.jpg");
-    height: auto;
-    width: auto;
-  }
-  h2{
-    color: white;
-  }
-</style>
-
-  <main class="d-flex align-items-center justify-content-center height-100">
-          <div class="content">
-               <header class="text-center mt-5">
-                    <h2>Login!</h2>
-                   
-               </header>
-               <form class="login-form bg-dark-alpha p-5 text-white" action="<?=FRONT_ROOT?>/home/login" method="POST">
-                    <div class="form-group">
-                         <label for="">Usuario</label>
-                         <input type="text" name="email" class="form-control form-control-lg" placeholder="Ingresar usuario">
-                    </div>
-                    <div class="form-group">
-                         <label for="">Contraseña</label>
-                         <input type="password" name="password" class="form-control form-control-lg" placeholder="Ingresar contraseña">
-                    </div>
-                    <button class="btn btn-dark btn-block btn-lg mt-5" type="submit">
-                         Iniciar Sesión
-                    </button>
-               </form>
-          </div>
-     </main>
-
-    <?php require(FOOTER); ?>
-
-   
-    
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  </body>
-
-</html>
--- app/views/signup.php
+++ app/views/signup.php
@@ -1,62 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-
 <?php require(HEADER); ?>
 
-  <body>
+<main class="d-flex align-items-center justify-content-center height-100">
+    <div class="content">
+        <header class="text-center mt-5">
+            <h2>Registrate aquí</h2> 
+        </header>
+        <form class="login-form bg-dark-alpha p-5 text-white" action="<?=FRONT_ROOT?>/home/add-user" method="POST">
+            <div class="form-group">
+                 <label for="">Email</label>
+                 <input type="email" name="email" class="form-control form-control-lg" placeholder="Ingresar email">
+            </div>
+            <div class="form-group">
+                 <label for="">Contraseña</label>
+                 <input type="password" name="password" class="form-control form-control-lg" placeholder="Ingresar contraseña">
+            </div>
+            <div class="form-group">
+                 <label for="">Nombre</label>
+                 <input type="text" name="name" class="form-control form-control-lg" placeholder="Ingresar nombre">
+            </div>
+            <div class="form-group">
+                 <label for="">Apellido</label>
+                 <input type="text" name="lastname" class="form-control form-control-lg" placeholder="Ingresar apellido">
+            </div>
+            <button class="btn btn-dark btn-block btn-lg mt-5" type="submit">
+                 Registrarse
+            </button>
+        </form>
+    </div>
+</main>
 
- <?php require(NAVBAR); ?>
-
-<style type="text/css">
-  main{
-    background-image: url("<?= IMG_FRONT_ROOT?>/fondologin.jpg");
-    height: auto;
-    width: auto;
-  }
-  h2{
-    color: white;
-  }
-</style>
-
-  <main class="d-flex align-items-center justify-content-center height-100">
-          <div class="content">
-               <header class="text-center mt-5">
-                    <h2>Registrate!</h2>
-                   
-               </header>
-               <form class="login-form bg-dark-alpha p-5 text-white" action="<?=FRONT_ROOT?>/home/add-user" method="POST">
-                    <div class="form-group">
-                         <label for="">Email</label>
-                         <input type="email" name="email" class="form-control form-control-lg" placeholder="Ingresar email">
-                    </div>
-                    <div class="form-group">
-                         <label for="">Contraseña</label>
-                         <input type="password" name="password" class="form-control form-control-lg" placeholder="Ingresar contraseña">
-                    </div>
-                    <div class="form-group">
-                         <label for="">Nombre</label>
-                         <input type="text" name="name" class="form-control form-control-lg" placeholder="Ingresar nombre">
-                    </div>
-                     <div class="form-group">
-                         <label for="">Apellido</label>
-                         <input type="text" name="lastname" class="form-control form-control-lg" placeholder="Ingresar apellido">
-                    </div>
-                    <button class="btn btn-dark btn-block btn-lg mt-5" type="submit">
-                         Registrarse
-                    </button>
-               </form>
-          </div>
-     </main>
-
-    <?php require(FOOTER); ?>
-
-   
-    
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  </body>
-
-</html>
+<?php require(FOOTER); ?>
