59ed5532159ab0e4b8990213025fb8a372dfc5c2
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-27 19:59:13 -0300

    href fix in navbar, points to 'register' instead of 'signup'

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -4,7 +4,7 @@
         <div class="right">
             <?php if(!isset($_SESSION['userLogedIn'])) : ?>
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/loginScreen">Ingresar</a>
-                <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/signup">Registrarse</a>
+                <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/register">Registrarse</a>
             <?php else :  ?>
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/logout">Salir</a>
             <?php endif; ?>  
