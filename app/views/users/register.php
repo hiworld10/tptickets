4e78eb8636b379c9709bd1427de2d0f70715d521
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-27 19:40:18 -0300

    Password fields are not auto filled in register form

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -18,23 +18,23 @@
         <form class="login-form bg-dark-alpha p-5 text-white" action="<?= FRONT_ROOT ?>/users/register" method="POST">
             <div class="form-group">
                  <label for="">Nombre</label>
-                 <input type="text" name="name" class="form-control form-control-lg" placeholder="Ingresar nombre" value="<?php echo $data['name'] ?>">
+                 <input type="text" name="name" class="form-control form-control-lg" placeholder="Ingresar nombre" value="<?php echo $data['name'] ?>" autofocus required>
             </div>
             <div class="form-group">
                  <label for="">Apellido</label>
-                 <input type="text" name="surname" class="form-control form-control-lg" placeholder="Ingresar apellido" value="<?php echo $data['surname'] ?>">
+                 <input type="text" name="surname" class="form-control form-control-lg" placeholder="Ingresar apellido" value="<?php echo $data['surname'] ?>" required>
             </div>            
             <div class="form-group">
                  <label for="">Email</label>
-                 <input type="email" name="email" class="form-control form-control-lg" placeholder="Ingresar email" value="<?php echo $data['email'] ?>">
+                 <input type="email" name="email" class="form-control form-control-lg" placeholder="Ingresar email" value="<?php echo $data['email'] ?>" required>
             </div>
             <div class="form-group">
                  <label for="">Contraseña (min. 6 caracteres)</label>
-                 <input type="password" name="password" class="form-control form-control-lg" placeholder="Ingresar contraseña" value="<?php echo $data['password'] ?>">
+                 <input type="password" name="password" class="form-control form-control-lg" placeholder="Ingresar contraseña" value="" required>
             </div>
             <div class="form-group">
                  <label for="">Confirmar contraseña</label>
-                 <input type="password" name="confirm_password" class="form-control form-control-lg" placeholder="Ingresar contraseña" value="<?php echo $data['confirm_password'] ?>">
+                 <input type="password" name="confirm_password" class="form-control form-control-lg" placeholder="Ingresar contraseña" value="" required>
             </div>            
             <button class="btn btn-dark btn-block btn-lg mt-5" type="submit">
                  Registrarse
