765667bc91e3c8eb220dee85d6b7c072e1bf53ab
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-26 23:33:53 -0300

    Add own users view folder, add register view

---
+++ app/views/users/register.php
@@ -0,0 +1,36 @@
+<?php require(HEADER); ?>
+
+<main class="d-flex align-items-center justify-content-center height-100">
+    <div class="content">
+        <header class="text-center mt-5">
+            <h2>Registrate aquí</h2> 
+        </header>
+        <form class="login-form bg-dark-alpha p-5 text-white" action="<?= FRONT_ROOT ?>/users/register" method="POST">
+            <div class="form-group">
+                 <label for="">Nombre</label>
+                 <input type="text" name="name" class="form-control form-control-lg" placeholder="Ingresar nombre">
+            </div>
+            <div class="form-group">
+                 <label for="">Apellido</label>
+                 <input type="text" name="lastname" class="form-control form-control-lg" placeholder="Ingresar apellido">
+            </div>            
+            <div class="form-group">
+                 <label for="">Email</label>
+                 <input type="email" name="email" class="form-control form-control-lg" placeholder="Ingresar email">
+            </div>
+            <div class="form-group">
+                 <label for="">Contraseña</label>
+                 <input type="password" name="password" class="form-control form-control-lg" placeholder="Ingresar contraseña">
+            </div>
+            <div class="form-group">
+                 <label for="">Confirmar contraseña</label>
+                 <input type="password" name="password" class="form-control form-control-lg" placeholder="Ingresar contraseña">
+            </div>            
+            <button class="btn btn-dark btn-block btn-lg mt-5" type="submit">
+                 Registrarse
+            </button>
+        </form>
+    </div>
+</main>
+
+<?php require(FOOTER); ?>
