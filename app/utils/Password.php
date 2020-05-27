38879743d6ff92a067feda16d3c906326c8a888b
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-27 00:36:22 -0300

    Add Password util class

---
+++ app/utils/Password.php
@@ -0,0 +1,26 @@
+<?php 
+
+namespace app\utils;
+
+/**
+ * Clase utilitaria que provee herramientas relacionadas con contraseñas, tales como cifrado, verificacion, etc.
+ */
+class Password
+{
+    public static function verify($string, $hash) {
+        return (password_verify($string, $hash)) ? true : false;
+    }
+
+    public static function hasLength($string, $length) {
+        return (strlen($string) < $length) ? true : false;
+    }
+
+    public static function hash($string) {
+        return password_hash($string, PASSWORD_DEFAULT);
+    }
+
+    public static function match($str1, $str2) {
+        return ($str1 === $str2) ? true : false;
+    }
+}
+ ?>
