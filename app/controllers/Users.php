4348b5272a1c984ea1e59c436e5add9c329272a9
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-28 20:48:54 -0300

    Move createSession() and destroySession() to Auth class

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -154,37 +154,6 @@ class Users extends \core\Controller {
         $this->destroySession();
         $this->redirect('');
     }
-
-    private function createSession($user) {
-        session_regenerate_id(true);
-        $_SESSION['user_id'] = $user->getId();
-        $_SESSION['user_name'] = $user->getName();
-        if ($user->getAdmin() == 'true') {
-            echo "Admin is true";
-            $_SESSION['is_admin'] = $user->getAdmin();
-        }
-    }
-
-    private function destroySession() {
-        //Desligar todas las variables en $_SESSION
-        $_SESSION = [];
-        //Borrar la cookie en sesion
-        if (ini_get("session.use_cookies")) {
-            $params = session_get_cookie_params();
-
-            setcookie(
-                session_name(), 
-                '', 
-                time() - 42000,
-                $params['path'], 
-                $params['domain'],
-                $params['secure'], 
-                $params['httponly']
-            );
-        }
-        //Finalmente, destruir la sesion
-        session_destroy();
-    }
 }
 
 ?>
