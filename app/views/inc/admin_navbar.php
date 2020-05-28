3c04246838a0fdb77c61edfa03a9057097cf8a12
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-28 12:44:45 -0300

    Change order of menu items in admin navbar

--- app/views/inc/admin_navbar.php
+++ app/views/inc/admin_navbar.php
@@ -1,12 +1,12 @@
 <div id="cssmenu" class="__full-height-perc">
     <ul>
         <li><a href="<?=FRONT_ROOT?>/artists" >Artistas</a></li>
+        <li><a href="<?=FRONT_ROOT?>/calendars" title="...">Calendarios</a></li>
         <li><a href="<?=FRONT_ROOT?>/categories"  title="...">Categorias</a></li>
         <li><a href="<?=FRONT_ROOT?>/events" title="...">Eventos</a></li>
-        <li><a href="<?=FRONT_ROOT?>/calendars" title="...">Calendario</a></li>
+        <li><a href="<?=FRONT_ROOT?>/place-events" title="...">Lugares</a></li>
         <li><a href="<?=FRONT_ROOT?>/event-seats" title="...">Plazas</a></li>
         <li><a href="<?=FRONT_ROOT?>/seat-types" title="...">Tipo Plazas</a></li>
-        <li><a href="<?=FRONT_ROOT?>/place-events" title="...">Lugar</a></li>
         <li><a href="<?=FRONT_ROOT?>/users" title="...">Usuarios</a></li>
     </ul>
 </div>
