6d4e2867deda1cf42dd203553610fc979e5c539b
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-27 23:14:45 -0300

    Navbar now shows the logged in user name and logout option if user is in session, else it displays the sign up and login buttons

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -2,10 +2,11 @@
     <div class="container">
         <a class="navbar-brand" href="<?= FRONT_ROOT ?>/home/">TPTickets</a>
         <div class="right">
-            <?php if(!isset($_SESSION['userLogedIn'])) : ?>
+            <?php if(!isset($_SESSION['user_id'])) : ?>
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/login">Ingresar</a>
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/register">Registrarse</a>
             <?php else :  ?>
+                <a class="btn btn-primary" href="<?= FRONT_ROOT ?>">Hola, <?php echo htmlspecialchars($_SESSION['user_name']) ?></a>
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/logout">Salir</a>
             <?php endif; ?>  
         </div>
