9d34e839b2e2dcf7fa580d6e701315a404c974cb
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-05-28 21:23:19 -0300

    Change redirect in requireAdminLogin() to home index. Add description to the Authentication class

--- app/controllers/Authentication.php
+++ app/controllers/Authentication.php
@@ -4,8 +4,15 @@ namespace app\controllers;
 
 use app\Auth;
 
+/**
+ * Clase abstracta base que extiende del core Controller, hace que cualquier controladora que la extenda pueda requerir que exista un usuario en sesión previo a ejectuar cualquier método propio. Ideal para ocultar funcionalidades de cualquier tipo, a nivel usuario regular o administrador.
+ */
 abstract class Authentication extends \core\Controller
 {   
+    /**
+     * Redirecciona a pagina de login si no encuentra un usuario regular en sesión.
+     * @return [type] [description]
+     */
     protected function requireUserLogin()
     {
         if (!Auth::isUserLoggedIn()) {
@@ -13,10 +20,14 @@ abstract class Authentication extends \core\Controller
         }
     }
 
+    /**
+     * Redirecciona a la página de inicio si no encuentra un administrador en sesión. A contrario de requireUserLogin(), envía a la página de inicio sin dar pista de que el link introducido pueda tratarse de uno que requiera de privilegios para accederlo.
+     * @return [type] [description]
+     */
     protected function requireAdminLogin()
     {
         if (!Auth::isAdminLoggedIn()) {
-            $this->redirect('users/login');
+            $this->redirect('');
         }
     }
 }
