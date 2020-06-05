9c5ded3e64fbbeb843c5ece03af61842bb2ebe7b
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-05 15:38:18 -0300

    Add container div and a dl-horizontal property to the dl tag (doesn't seem to change anything?) in users show view

--- app/views/users/show.php
+++ app/views/users/show.php
@@ -1,16 +1,15 @@
 <?php require HEADER; ?>
 
-<h1>Tu Perfil</h1>
-<br>
-<dl>
-    <dt>Nombre</dt>
-    <dd><?php echo htmlspecialchars($data['user']->getName()) ?></dd>
-    <dt>Apellido</dt>
-    <dd><?php echo htmlspecialchars($data['user']->getSurname()) ?></dd>
-    <dt>E-mail</dt>
-    <dd><?php echo htmlspecialchars($data['user']->getEmail()) ?></dd>
-</dl>
-
-<a href="<?= FRONT_ROOT ?>/users/edit/<?= $_SESSION['tptickets_user_id'] ?>">Editar perfil</a>
-
+<div class="container">
+    <h1>Tu Perfil</h1><br>
+    <dl class="dl-horizontal">
+        <dt>Nombre</dt>
+        <dd><?php echo htmlspecialchars($data['user']->getName()) ?></dd>
+        <dt>Apellido</dt>
+        <dd><?php echo htmlspecialchars($data['user']->getSurname()) ?></dd>
+        <dt>E-mail</dt>
+        <dd><?php echo htmlspecialchars($data['user']->getEmail()) ?></dd>
+    </dl>
+    <a href="<?= FRONT_ROOT ?>/users/edit/<?= $_SESSION['tptickets_user_id'] ?>">Editar perfil</a>
+</div>
 <?php require(FOOTER); ?>
