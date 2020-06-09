490dcd3b419647b80af73d1f402f40a4816857dc
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-09 14:04:32 -0300

    Add rough tags for list_seats view (needs improving)

--- app/views/calendars/list_seats.php
+++ app/views/calendars/list_seats.php
@@ -1,7 +1,21 @@
 <?php require HEADER ?>
 
-<?php echo '<pre>';
-print_r($data);
-echo '</pre>'; ?>
+<div>
+    <h1><?php echo htmlspecialchars($data['event_name']) ?></h1>
+    <br>
+    <?php echo "Fecha de evento: " . htmlspecialchars($data['date']) ?>
+</div>
+
+<div class="jumbotron">
+    <?php foreach ($data['event_seats'] as $event_seat): ?>
+        <div>
+            <pre><?php echo htmlspecialchars($event_seat->getSeatType()->getType()) ?></pre>
+            <pre><?php echo "Precio: $" . htmlspecialchars($event_seat->getPrice()) ?></pre>
+            <form action="" method="POST">
+                <button type="submit">Comprar</button>
+            </form>
+        </div>
+    <?php endforeach ?>
+</div>
 
 <?php require FOOTER ?>
