2979206bf706c689f468ae8c18a302d469b75e80
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-09 14:11:05 -0300

    Cleanup code of Home controller

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,19 +1,19 @@
-<?php 
+<?php
 
 namespace app\controllers;
 
 use core\Controller;
-use app\controllers\Users;
 
-class Home extends Controller {
+class Home extends Controller
+{
 
-    function __construct() {
-        $this->user_dao = $this->dao('User');
-        $this->calendar_dao = $this->dao('Calendar');
+    public function __construct()
+    {
         $this->event_dao = $this->dao('Event');
     }
 
-    public function index() {
+    public function index()
+    {
         if (isset($_SESSION['tptickets_is_admin'])) {
             $this->view('admin/admin');
         } else {
@@ -23,20 +23,9 @@ class Home extends Controller {
     }
 
     //busca por nombre artista, nombre evento, lugar
-    public function search($string) {
-
+    public function search($string)
+    {
         $data['events'] = $this->event_dao->retrieveActiveEventsByString($string);
-
         $this->view('home/search', $data);
-    }
-
-    public function showEvent($id_event) {
-        
-        $data['calendars'] = $this->calendar_dao->retrieveByEventId($id_event);
-        if(isset($data['calendars'])) {
-            $this->view('home/show_event', $data);
-        } else {
-            $this->index();
-        }
     }
 }
