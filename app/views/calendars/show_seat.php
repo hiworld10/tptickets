6e84c6f62cca16e2c1103a362182aeae0e8a518c
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-16 17:59:36 -0300

    Add additional 'price' hidden input in show_seat form and update amount input in which it requires a number to be entered and a minimum of 1

--- app/views/calendars/show_seat.php
+++ app/views/calendars/show_seat.php
@@ -12,9 +12,10 @@
             <pre><?php echo htmlspecialchars($data['event_seat']->getSeatType()->getType()) ?></pre>
             <pre><?php echo "Precio: $" . htmlspecialchars($data['event_seat']->getPrice()) ?></pre>
             <p>Seleccioná la cantidad de asientos (Máximo 5)</p>
-            <form name="form" action="<?= FRONT_ROOT ?>/purchases/add-new" method="POST">
+            <form name="form" action="<?= FRONT_ROOT ?>/purchases/add-new-line" method="POST">
                 <input type="hidden" name="id_event_seat" value="<?= $data['event_seat']->getId() ?>"><br>
-                <input type="number" max="5" name="amount"><br>
+                <input type="hidden" name="price" value="<?= $data['event_seat']->getPrice() ?>"><br>
+                <input type="number" min="1" max="5" name="amount" required><br>
                 <button type="submit">Añadir a compra</button>
             </form>
         </div>
