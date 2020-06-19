690f23c811853243f7f84099932745a049a948ce
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-19 14:49:35 -0300

    Navbar displays subtotal of purchase when at least one item was added to cart

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -6,6 +6,9 @@
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/login">Ingresar</a>
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/register">Registrarse</a>
             <?php else :  ?>
+                <?php if (isset($_SESSION['tptickets_purchase'])): ?>
+                    <a href="">Tu Compra: <?php echo htmlspecialchars('$' . number_format((float)$_SESSION['tptickets_purchase']['subtotal'], 2, ',', '')) ?></a>
+                <?php endif ?>
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/show/<?= $_SESSION['tptickets_user_id'] ?>">Mi cuenta</a>
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/logout">Salir</a>
             <?php endif; ?>  
