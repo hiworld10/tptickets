f94869e92654b0fbec18b9dd231961e5de3dd059
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-19 15:31:25 -0300

    Update href to point to Purchases showCart()

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -6,8 +6,8 @@
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/login">Ingresar</a>
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/register">Registrarse</a>
             <?php else :  ?>
-                <?php if (isset($_SESSION['tptickets_purchase'])): ?>
-                    <a href="">Tu Compra: <?php echo htmlspecialchars('$' . number_format((float)$_SESSION['tptickets_purchase']['subtotal'], 2, ',', '')) ?></a>
+                <?php if (isset($_SESSION['tptickets_items'])): ?>
+                    <a href="<?= FRONT_ROOT ?>/purchases/show-cart">Tu Compra: <?php echo htmlspecialchars('$' . number_format((float)$_SESSION['tptickets_subtotal'], 2, ',', '')) ?></a>
                 <?php endif ?>
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/show/<?= $_SESSION['tptickets_user_id'] ?>">Mi cuenta</a>
                 <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/logout">Salir</a>
