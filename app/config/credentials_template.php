bba52e9c48de75a61e57979c5c246ab334d86bc0
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-19 17:29:15 -0300

    Add new config/credentials_template.php, provides default defines for storing credentials and details how to include them to project

---
+++ app/config/credentials_template.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Esta es una plantilla para incluir datos de credenciales en el proyecto.
+ * Esto es necesario para poder disponer de una base de datos funcional, asi también como
+ * para poder enviar e-mails.
+ *
+ * Para poder hacer uso de ella, copie los contenidos y descoméntelos en un nuevo archivo 
+ * e inclúyalo en el index.php mediante require().
+ *
+ * JAMAS INCLUIR UN ARCHIVO CON CREDENCIALES COMO PARTE DE UN REPOSITORIO GIT.
+ * Utilice un .gitignore apropiadamente.
+ * También puede ser preferible ubicar el archivo fuera de la raíz del proyecto.
+ */
+
+// Base de datos
+//
+// define('DB_HOST', '');
+// define('DB_NAME', '');
+// define('DB_USER', '');
+// define('DB_PASS', '');
+// 
+// Proveedor de servicio de e-mail
+// 
+// <<completar datos aqui>>
