459ee68bdbdaa45a3c21c96ea5ccdc501721fcc1
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-19 15:30:22 -0300

    Purchases controller adds new line in session to cart and can display the contents of it (only a print_r(), needs to improve)

--- app/controllers/Purchases.php
+++ app/controllers/Purchases.php
@@ -2,10 +2,55 @@
 
 namespace app\controllers;
 
+use app\utils\Flash;
+
 class Purchases extends \app\controllers\Authentication
 {
     public function __construct()
     {
-        $this->purchases_dao = $this->dao('Purchases');
-    }        
+        $this->requireUserLogin();
+        $this->purchase_dao = $this->dao('Purchase');
+    }
+
+    public function addNewLine()
+    {
+        $this->redirectIfRequestIsNotPost('');
+
+        if ($this->purchase_dao->addNewLineInSession($_POST)) {
+            Flash::addMessage('El item fue añadido a tu carro de compra exitosamente.');
+            $this->redirect('');    
+        } else {
+            Flash::addMessage('No hemos podido procesar la operación en este momento. Intentalo más tarde.', Flash::WARNING);            
+            $this->redirect('');    
+        }
+        //Contenido de las lineas de compra en sesión
+        /*echo '<pre>';
+        print_r($_SESSION['tptickets_items']);
+        echo '</pre>';*/
+    }
+
+    public function removeLine($id_event_seat)
+    {
+        $this->redirectIfRequestIsNotPost('');
+    }
+
+    public function checkout()
+    {
+        
+    }
+
+    public function empty()
+    {
+        if (isset($_SESSION['tptickets_items'])) {
+            unset($_SESSION['tptickets_items']);
+        }
+    }
+
+    public function showCart()
+    {
+        $data['items'] = $_SESSION['tptickets_items'];
+        $data['subtotal'] = $_SESSION['tptickets_subtotal'];
+
+        $this->view('purchases/show_cart', $data);
+    }
 }
