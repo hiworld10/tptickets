f5ef320431716247d1fbe625982230d78f3f6cf1
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-20 17:37:53 -0300

    show_cart view now displays items with its respective properties and can remove them individually

--- app/views/purchases/show_cart.php
+++ app/views/purchases/show_cart.php
@@ -2,12 +2,30 @@
 
 <h1>Tu compra</h1>
 
-<pre>
-    <?php foreach ($data['items'] as $item): ?>
-        <?php print_r($item); ?>
-    <?php endforeach ?>
-</pre>
-
-<p>Subtotal: $<?php echo htmlspecialchars(number_format($data['subtotal'], 2, ',', '')) ?></p>
+<?php if (!empty($data['items'])): ?>
+	<?php foreach ($data['items'] as $item): ?>
+		<div class="jumbotron">
+			<div>
+				<div>
+					<big><big><big><?php echo htmlspecialchars($item['event_name']) ?></big></big></big><br>
+					<big>Fecha: <?php echo htmlspecialchars($item['date']) ?></big><br>
+					<big>Cantidad: <?php echo htmlspecialchars($item['amount']) ?></big><br>
+					<big>Precio (c/u): <?php echo htmlspecialchars(number_format($item['subtotal'], 2, ',', '')) ?></big><br>
+					<form name="form" action="<?= FRONT_ROOT ?>/purchases/remove-line" method="POST">
+						<input type="hidden" name="id_event_seat" value="<?= $item['id_event_seat'] ?>">
+						<button type="submit">Eliminar item</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	<?php endforeach ?>
+	<div class="jumbotron">
+		<big><big>Total compra: $<?php echo htmlspecialchars(number_format($data['subtotal'], 2, ',', '')) ?></big></big>
+	</div>
+<?php else: ?>
+	<div class="jumbotron">
+		<big>Aún no tenés items añadidos a tu carro. Buscá eventos disponibles <a href="<?= FRONT_ROOT ?>">aquí.</a></big>
+	</div>
+<?php endif ?>
 
 <?php require FOOTER ?>
