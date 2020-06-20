c6b7708189695a12a8e8ae5de0b858a3cb423277
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-20 17:09:30 -0300

    Code format in PurchaseDAO

--- app/dao/db/PurchaseDAO.php
+++ app/dao/db/PurchaseDAO.php
@@ -1,8 +1,6 @@
-<?php 
+<?php
 
 namespace app\dao\db;
-
-use app\dao\db\EventSeatDAO;
 
 class PurchaseDAO
 {
@@ -10,7 +8,7 @@ class PurchaseDAO
     public function __construct()
     {
         if (!isset($_SESSION['tptickets_items']) || !isset($_SESSION['tptickets_subtotal'])) {
-            $_SESSION['tptickets_items'] = [];
+            $_SESSION['tptickets_items']    = [];
             $_SESSION['tptickets_subtotal'] = 0;
         }
     }
@@ -18,18 +16,18 @@ class PurchaseDAO
     public function addNewLineInSession($data)
     {
         $subtotal = $data['price'] * $data['amount'];
+
         $_SESSION['tptickets_items'][] = [
-                                                'id_event_seat' => $data['id_event_seat'],
-                                                'amount' => $data['amount'],
-                                                'price' => $data['price'],
-                                                'subtotal' => $subtotal
-                                            ];
+            'id_event_seat' => $data['id_event_seat'],
+            'amount'        => $data['amount'],
+            'price'         => $data['price'],
+            'subtotal'      => $subtotal,
+            'event_name'    => $data['event_name'],
+            'date'          => $data['date'],
+        ];
         $_SESSION['tptickets_subtotal'] += $subtotal;
-        
-        return !empty($_SESSION['tptickets_items']) ? true : false;                           
+
+        return !empty($_SESSION['tptickets_items']) ? true : false;
     }
 
-
 }
-
- ?>
