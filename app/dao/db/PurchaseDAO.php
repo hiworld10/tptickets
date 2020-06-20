cb9beed902fa691a2c606f9a06146e8399824aaf
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-20 17:10:40 -0300

    Add removeLineInSession() in PurchaseDAO

--- app/dao/db/PurchaseDAO.php
+++ app/dao/db/PurchaseDAO.php
@@ -30,4 +30,22 @@ class PurchaseDAO
         return !empty($_SESSION['tptickets_items']) ? true : false;
     }
 
+    public function removeLineInSession($id_event_seat)
+    {
+        $deleted = false;
+
+        foreach ($_SESSION['tptickets_items'] as $key => $value) {
+
+            if ($id_event_seat == $value['id_event_seat']) {
+
+                $_SESSION['tptickets_subtotal'] -= $value['subtotal'];
+                unset($_SESSION['tptickets_items'][$key]);
+                $deleted = true;
+                break;
+            }
+        }
+
+        return $deleted;
+    }
+
 }
