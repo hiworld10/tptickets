dc10750ca74c7f26f4aa9d74afd3f61b12425747
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-22 11:50:28 -0300

    Add admin/tickets view. Only shows the entire list (without qr codes)

---
+++ app/views/admin/tickets.php
@@ -0,0 +1,31 @@
+<?php require HEADER ?>
+
+<div id="container" class="__full-height-perc">
+    <?php require ADMIN_NAVBAR ?>
+    <div id="divform"  class="__full-height-perc">
+
+        <div id="table">
+            <table class="table bg-light-alpha">
+
+                <?php if (isset($data['tickets'])): ?>
+                    <thead>     
+                        <th>ID</th>  
+                        <th>Linea compra</th>
+                        <th>Cantidad</th>    
+                    </thead>
+                    <tbody>
+                        <?php foreach ($data['tickets'] as $value): ?>
+                            <tr>
+                                <td><?= $value->getId() ?></td>
+                                <td><?= $value->getPurchaseLineId() ?></td>
+                                <td><?= $value->getNumber() ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                <?php endif; ?>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?php require FOOTER ?>
