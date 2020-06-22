a6cfcd02041f5cdfcdc8dd643dd45dcb98c7e173
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-22 11:42:33 -0300

    admin/purchases and admin/purchase_lines views only list all respective items

--- app/views/admin/purchase_lines.php
+++ app/views/admin/purchase_lines.php
@@ -1,163 +1,35 @@
-<?php require HEADER; ?>
+<?php require HEADER ?>
 
- <div id="container" class="__full-height-perc">
-  <?php require ADMIN_NAVBAR ?>
+<div id="container" class="__full-height-perc">
+    <?php require ADMIN_NAVBAR ?>
+    <div id="divform"  class="__full-height-perc">
 
+        <div id="table">
+            <table class="table bg-light-alpha">
 
-
-  <div id="divform" class="__full-height-perc">
-
-    <?php if(isset($purcharseline)){ ?>
-
-
-      <form name='formulario' action="<?=FRONT_ROOT?>/admin/purchase-lines/updatePurcharseLine"  method="POST">
-       <div class="form-row">
-        <div class="col-md-1 mb-2 mb-md-0 form-row">
-          <label>Id</label>
-          <input type="text" name="id" class="form-control form-control-lg" value="<?= $purcharseline->getId(); ?>" readonly>
+                <?php if (isset($data['purchase_lines'])): ?>
+                    <thead>     
+                        <th>ID</th>  
+                        <th>ID Plaza</th>    
+                        <th>ID compra</th>
+                        <th>Cantidad</th>
+                        <th>Precio</th>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($data['purchase_lines'] as $value): ?>
+                            <tr>
+                                <td><?= $value->getId() ?></td>
+                                <td><?= $value->getEventSeatId() ?></td>
+                                <td><?= $value->getPurchaseId() ?></td>
+                                <td><?= $value->getQuantity() ?></td>
+                                <td><?= $value->getPrice() ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                <?php endif; ?>
+            </table>
         </div>
-        <div class="col-md-5 mb-2 mb-md-0 form-row">
-          <label>Monto</label>
-          <input type="date" name="amount" class="form-control form-control-lg" value="<?= $purcharseline->getAmount(); ?>" required>
-        </div>
-        <div class="col-md-5 mb-2 mb-md-0 form-row">
-          <label>IdTicket</label>
-          <select class="form-control" name="ticket" required>
-            <?php if($ticketArray){ 
-             foreach ($ticketArray as $key => $value) { 
-               ?>
-               <option value="<?= $value->getId(); ?>"><?= $value->getId(); ?></option> 
-             <?php }
-           }else{ ?>
-            <option >NO HAY TICKETS</option>
-          <?php } ?>
-        </select>
-      </div>
-        <div class="col-md-5 mb-2 mb-md-0 form-row">
-          <label>IdCompra</label>
-          <select class="form-control" name="purchase" required>
-            <?php if($purchaseArray){ 
-             foreach ($purchaseArray as $key => $value) { 
-               ?>
-               <option value="<?= $value->getId(); ?>"><?= $value->getId(); ?></option> 
-             <?php }
-           }else{ ?>
-            <option >NO HAY COMPRAS</option>
-          <?php } ?>
-        </select>
-      </div>
-      <div class="col-12 col-md-2 form-row">
-        <button type="submit" class="btn btn-block btn-lg btn-primary">Aceptar</button>
-      </div>
     </div>
-  </form>
-
-<?php   }else{  ?>
-
-  <form name='formulario' action="<?=FRONT_ROOT?>/admin/purchase-lines/addPurchaseLine"  method="POST">
-   <div class="form-row p-2">
-    <div class="form-group col-12 col-md-9 mb-2 mb-md-0">
-      <input type="text"   class="form-control form-control-lg" name="amount" placeholder="Ingrese monto..." required>
-    </div>
-    <div class="col-lg-4 form-group">
-     <label for="">Ticket</label>
-     <select class="form-control" name="ticket" required>
-      <?php if($ticketArray){ 
-       foreach ($ticketArray as $key => $value) { 
-         ?>
-         <option value="<?= $value->getId(); ?>"><?= $value->getId(); ?></option> 
-       <?php }
-     }else{ ?>
-      <option >NO HAY TICKETS</option>
-    <?php } ?>
-    </select>
-  </div>
-  <div class="col-lg-4 form-group">
-     <label for="">Compra</label>
-     <select class="form-control" name="event" required>
-      <?php if($purchaseArray){ 
-       foreach ($purchaseArray as $key => $value) { 
-         ?>
-         <option value="<?= $value->getId(); ?>"><?= $value->getId(); ?></option> 
-       <?php }
-     }else{ ?>
-      <option >NO HAY COMPRAS</option>
-    <?php } ?>
-    </select>
-  </div>
-<?php if($ticketArray && $purcheseArray) { ?>
-  <div class="col-11 col-md-3 mt-4">
-    <button type="submit" class="btn btn-block btn-lg btn-primary">Agregar</button>
-  </div>
-<?php }else{ ?>
-  <div class="col-11 col-md-3 mt-4">
-    <button type="submit" disabled class="btn btn-block btn-lg btn-primary">Agregar</button>
-  </div>
-<?php } ?>
-
-</div>
-</form>
-
-
-<?php } ?>
-
-<div id="table" class="__full-height-perc">
-  <table class="table bg-light-alpha">
-
-    <?php if(!empty($purchaseLineArray)) { ?>
-      <thead>     
-       <th>Id</th>  
-       <th>Monto</th>     
-       <th>Ticket</th>  
-       <th>Compra</th>         
-     </thead>
-     <tbody>
-      <?php foreach ($purchaseLineArray as $value) { ?>
-        <tr>
-         <td><?= $value->getId(); ?></td>
-         <td><?= $value->getAmount(); ?></td>
-         <td><?= $value->getTicket()->getId(); ?></td>
-         <td><?= $value->getPurchaseId(); ?></td>
-
-         <td>
-          <form action="<?=FRONT_ROOT?>/admin/purchase-lines/deletePurchaseLine" method="POST">
-            <button name="iddelete" value="<?= $value->getId();  ?>"id="boton1" type="submit"class="btn btn-block btn-lg btn-primary btn-sm">Eliminar</button>
-          </form>
-        </td>
-        <td>
-          <form action="<?=FRONT_ROOT?>/admin/purchase-lines/getPurchaseLine" method="POST">
-            <button name="update" value="<?= $value->getId(); ?>" id="boton1" type="submit" class="btn btn-block btn-lg btn-primary btn-sm">Editar</button></td>
-          </form>
-        </td>
-      </tr>
-    <?php } ?>
-
-  </tbody>
-<?php  } ?>
-</table>
-
 </div>
 
-</div>
-
-</div>
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-<!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-</body>
-
-</html>
+<?php require FOOTER ?>
--- app/views/admin/purchases.php
+++ app/views/admin/purchases.php
@@ -1,116 +1,38 @@
-<?php require HEADER; ?>
+<?php require HEADER ?>
 
- <div id="container" class="__full-height-perc">
-  <?php require ADMIN_NAVBAR ?>
+<div id="container" class="__full-height-perc">
+    <?php require ADMIN_NAVBAR ?>
+    <div id="divform"  class="__full-height-perc">
 
+        <div id="table">
+            <table class="table bg-light-alpha">
 
-
-  <div id="divform" class="__full-height-perc">
-
-    <?php if(isset($purchase)){ ?>
-
-
-      <form name='formulario' action="<?=FRONT_ROOT?>/admin/purchases/updatePurchase"  method="POST">
-       <div class="form-row">
-        <div class="col-md-1 mb-2 mb-md-0 form-row">
-          <label>Id</label>
-          <input type="text" name="id" class="form-control form-control-lg" value="<?= $purchase->getId(); ?>" readonly>
+                <?php if (isset($data['purchases'])): ?>
+                    <thead>     
+                        <th>ID</th>  
+                        <th>ID Cliente</th>
+                        <th>ID Linea(s) Compra</th>
+                        <th>Fecha</th>
+                        <!-- <th>Linea compra</th> -->
+                    </thead>
+                    <tbody>
+                        <?php foreach ($data['purchases'] as $value): ?>
+                            <tr>
+                                <td><?= $value->getId() ?></td>
+                                <td><?= $value->getClientId() ?></td>
+                                <td>
+                                    <?php foreach ($value->getPurchaseLineArr() as $purchase_line): ?>
+                                        <?php print_r($purchase_line->getId()." - "); ?>  
+                                    <?php endforeach ?>
+                                </td>
+                                <td><?= $value->getDate() ?></td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                <?php endif ?>
+            </table>
         </div>
-        <div class="col-md-5 mb-2 mb-md-0 form-row">
-          <label>Fecha</label>
-          <input type="date" name="date" class="form-control form-control-lg" value="<?= $purchase->getDate(); ?>" required>
-        </div>
-        <div class="col-md-5 mb-2 mb-md-0 form-row">
-          <label>LineaCompra</label>
-              <input type="" name="purchaseline" class="form-control form-control-lg" value="<?= $purchase->getPurchaseLine(); ?>" required>
-      </div>
-      <div class="col-12 col-md-2 form-row">
-        <button type="submit" class="btn btn-block btn-lg btn-primary">Aceptar</button>
-      </div>
     </div>
-  </form>
-
-<?php   }else{  ?>
-
-  <form name='formulario' action="<?=FRONT_ROOT?>/admin/purchases/addPurchaseLine"  method="POST">
-   <div class="form-row p-2">
-    <div class="form-group col-12 col-md-9 mb-2 mb-md-0">
-      <input type="date"   class="form-control form-control-lg" name="date" placeholder="Ingrese fecha..." required>
-    </div>
-    <div class="col-lg-4 form-group">
-      
-     <label for="">LineaCompra</label>
-     <input type=""   class="form-control form-control-lg" name="purchaseline" placeholder="Ingrese lineas de compra..." required>
-     
-    </div>
-
-  <div class="col-11 col-md-3 mt-4">
-    <button type="submit" class="btn btn-block btn-lg btn-primary">Agregar</button>
-  </div>
-
-
-
-</div>
-</form>
-
-
-<?php } ?>
-
-<div id="table" class="__full-height-perc">
-  <table class="table bg-light-alpha">
-
-    <?php if(!empty($purchaseArray)) { ?>
-      <thead>     
-       <th>Id</th>  
-       <th>Fecha</th>     
-       <th>LineaCompra</th>          
-     </thead>
-     <tbody>
-      <?php foreach ($purchaseArray as $value) { ?>
-        <tr>
-         <td><?= $value->getId(); ?></td>
-         <td><?= $value->getDate(); ?></td>
-         <td><?= $value->getPurchaseLine(); ?></td><!-- SI ES UN OBJETO HAY Q AGREGAR (getId()) -->
-
-         <td>
-          <form action="<?=FRONT_ROOT?>/admin/purchases/deletePurchase" method="POST">
-            <button name="iddelete" value="<?= $value->getId();  ?>"id="boton1" type="submit"class="btn btn-block btn-lg btn-primary btn-sm">Eliminar</button>
-          </form>
-        </td>
-        <td>
-          <form action="<?=FRONT_ROOT?>/admin/purchases/getPurchase" method="POST">
-            <button name="update" value="<?= $value->getId(); ?>" id="boton1" type="submit" class="btn btn-block btn-lg btn-primary btn-sm">Editar</button></td>
-          </form>
-        </td>
-      </tr>
-    <?php } ?>
-
-  </tbody>
-<?php  } ?>
-</table>
-
 </div>
 
-</div>
-
-</div>
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
-<!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-</body>
-
-</html>
+<?php require FOOTER ?>
