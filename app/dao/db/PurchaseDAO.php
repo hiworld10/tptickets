fbd064fc7ed922d3911e9cc4f5c00efb4733cbdb
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-21 23:45:21 -0300

    CRUD functions implemented in PurchaseDAO

--- app/dao/db/PurchaseDAO.php
+++ app/dao/db/PurchaseDAO.php
@@ -2,15 +2,27 @@
 
 namespace app\dao\db;
 
-class PurchaseDAO
+use app\dao\IDAO;
+use app\dao\db\Connection;
+use app\models\Purchase;
+
+class PurchaseDAO implements IDAO
 {
+    private $tableName = "purchases";
+    private $connection;
+
     public function __construct()
-    {
+    {   
+        // Inicialización de carro de compra
         if (!isset($_SESSION['tptickets_items']) || !isset($_SESSION['tptickets_subtotal'])) {
             $_SESSION['tptickets_items']    = [];
             $_SESSION['tptickets_subtotal'] = 0;
         }
+
+        $this->connection = Connection::getInstance();
     }
+
+    // Shopping cart actions
 
     public function addNewLineInSession($data)
     {
@@ -71,5 +83,88 @@ class PurchaseDAO
             'items'    => $_SESSION['tptickets_items'],
             'subtotal' => $_SESSION['tptickets_subtotal'],
         ];
+    }
+
+    // DB access
+    
+    public function create($data)
+    {
+        try {
+            $query = "INSERT INTO " . $this->tableName . " (id_client) VALUES (:id_client)";
+
+            $parameters["id_client"] = $data['id_client'];
+
+            $this->connection->executeNonQuery($query, $parameters);
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    public function retrieveAll()
+    {
+        try {
+            $purchases = [];
+
+            $query = "SELECT * FROM " . $this->tableName;
+
+            $resultSet = $this->connection->execute($query);
+
+            foreach ($resultSet as $row) {
+                $purchase = new Purchase($row['id_purchase'], $row['id_client'], $row['date']);
+                array_push($purchases, $purchase);
+            }
+
+            return $purchases;
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    public function retrieveById($id)
+    {
+        try {
+            $purchase = null;
+
+            $query = "SELECT * FROM " . $this->tableName . " WHERE id_purchase = :id_purchase";
+
+            $parameters["id_purchase"] = $id;
+
+            $resultSet = $this->connection->execute($query, $parameters);
+
+            foreach ($resultSet as $row) {
+                $purchase = new Purchase($row['id_purchase'], $row['id_client'], $row['date']);
+            }
+
+            return $purchase;
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $query = "DELETE FROM " . $this->tableName . " WHERE id_purchase = :id_purchase";
+
+            $parameters["id_purchase"] = $id;
+
+            $this->connection->executeNonQuery($query, $parameters);
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+
+    public function update($data)
+    {
+        try {
+            $query = "UPDATE " . $this->tableName . " SET id_client = :id_client WHERE id_purchase = :id_purchase";
+
+            $parameters['id_client'] = $data['id_client'];
+            $parameters['id_purchase'] = $data['id_purchase'];
+
+            $this->connection->executeNonQuery($query, $parameters);
+        } catch (Exception $ex) {
+            throw $ex;
+        }
     }
 }
