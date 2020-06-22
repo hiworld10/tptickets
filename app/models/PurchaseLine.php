5bd48252933dbe134bc90f9fa491bd1886318c77
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-22 01:02:08 -0300

    Add missing properties in PurchaseLine model. Ticket class object property commented out for now

--- app/models/PurchaseLine.php
+++ app/models/PurchaseLine.php
@@ -7,16 +7,20 @@ use app\models\Ticket;
 class PurchaseLine
 {
     private $id;
-    private $amount;
-    private $ticket;
+    private $event_seat_id;
     private $purchase_id;
+    private $quantity;
+    private $price;
+    private $ticket;
 
-    public function __construct($id, $amount, Ticket $ticket, $purchase_id)
+    public function __construct($id, $event_seat_id, $quantity, $purchase_id, $price, /*Ticket*/ $ticket)
     {
-        $this->id          = $id;
-        $this->amount      = $amount;
-        $this->ticket      = $ticket;
-        $this->purchase_id = $purchase_id;
+        $this->id            = $id;
+        $this->event_seat_id = $event_seat_id;
+        $this->purchase_id   = $purchase_id;
+        $this->quantity      = $quantity;
+        $this->price         = $price;
+        $this->ticket        = $ticket;
     }
 
     public function getId()
@@ -24,9 +28,9 @@ class PurchaseLine
         return $this->id;
     }
 
-    public function getAmount()
+    public function getQuantity()
     {
-        return $this->amount;
+        return $this->quantity;
     }
 
     public function getTicket()
