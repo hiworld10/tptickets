d9fb5695e3a0275e039e7307a2d38f562abdb250
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-23 19:51:35 -0300

    Spaces between defines and php tags

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>TPTickets</title>
-    <link rel="stylesheet" href="<?=FRONT_ROOT?>/bootstrap-4.4.1-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="<?=FRONT_ROOT?>/css/admin.css">
+    <link rel="stylesheet" href="<?= FRONT_ROOT ?>/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= FRONT_ROOT ?>/css/admin.css">
     <style type="text/css">
         main {
-            background-image: url("<?= IMG_FRONT_ROOT?>/fondologin.jpg");
+            background-image: url("<?= IMG_FRONT_ROOT ?>/fondologin.jpg");
             height: auto;
             width: auto;
         }
