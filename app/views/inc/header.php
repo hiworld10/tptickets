12bbd493f47b12f8da5929b6d47dc3c5efeecbce
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-23 19:57:11 -0300

    move bootstrap min css and js files to public js and css folder. Update header href

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>TPTickets</title>
-    <link rel="stylesheet" href="<?= FRONT_ROOT ?>/bootstrap-4.4.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= FRONT_ROOT ?>/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?= FRONT_ROOT ?>/css/admin.css">
     <style type="text/css">
         main {
