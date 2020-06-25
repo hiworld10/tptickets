4ab2699cb52ef7d4297900525d4b9c3423cb8578
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-25 20:39:30 -0300

    Code format of core Controller

--- core/Controller.php
+++ core/Controller.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace core;
 
@@ -7,57 +7,60 @@ use \app\utils\StringUtils;
 /**
  * Controladora base de la cual otras extenderán. Provee las funcionalidades básicas de carga de objetos con acceso a modelos (DAO) y vistas.
  */
-
-abstract Class Controller {
-
-    protected function dao($dao) {
-        $dao = StringUtils::convertToStudlyCaps($dao) . 'DAO';
+abstract class Controller
+{
+    protected function dao($dao)
+    {
+        $dao       = StringUtils::convertToStudlyCaps($dao) . 'DAO';
         $namespace = 'app\dao\db\\';
-        $file = '../app/dao/db/' . $dao . '.php';
+        $file      = '../app/dao/db/' . $dao . '.php';
         if (file_exists($file)) {
             $class = $namespace . $dao;
             return new $class;
         }
     }
 
-    /*public function model($model) {
-        $model = StringUtils::convertToStudlyCaps($model);
+    public function model($model)
+    {
+        $model     = StringUtils::convertToStudlyCaps($model);
         $namespace = 'app\models\\';
-        $file = '../app/models/' . $model . '.php';
+        $file      = '../app/models/' . $model . '.php';
         if (file_exists($file)) {
             $class = $namespace . $model;
             return new $class;
-        }        
-    }*/
+        }
+    }
 
-    protected function view($view, $data = []) {
+    protected function view($view, $data = [])
+    {
         if (empty($view)) {
             $view = 'home/index';
         }
         $file = '../app/views/' . $view . '.php';
-        if(file_exists($file)) {
+        if (file_exists($file)) {
             require_once $file;
         } else {
             die('Error: la vista no pudo encontrarse en la ruta ' . $file);
         }
     }
 
-    protected function redirect($url = '/') {
+    protected function redirect($url = '/')
+    {
         header('Location: ' . FRONT_ROOT . $url, true, 303);
         exit;
     }
 
-    protected function redirectIfRequestIsNotPost($url = '/') {
+    protected function redirectIfRequestIsNotPost($url = '/')
+    {
         if ($_SERVER['REQUEST_METHOD'] != 'POST') {
             $this->redirect($url);
         }
     }
 
-    /*protected function redirectIfNoArgsArePassed($func_num_args, $url = '') {
+    protected function redirectIfNoArgsArePassed($func_num_args, $url = '')
+    {
         if (func_num_args() === 0) {
             $this->redirect($url);
         }
-    }*/     
+    }
 }
-
-?> 
