b94751e6380dbce0607081d4e55d94a3bed7a6b8
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-30 16:40:20 -0300

    Update purchase data template for email

--- app/views/mail/purchase_details_html.php
+++ app/views/mail/purchase_details_html.php
@@ -1,54 +1,33 @@
-<!--
-$data contents:
-
-['name']        // nombre del cliente
-
-['id_purchase'] // id de compra
-
-['total']       // total de compra
-
-['items'] ->
-    
-        [id_event_seat] => 10
-        [seat_type] => Campo VIP
-        [amount] => 2
-        [price] => 2000
-        [subtotal] => 4000
-        [event_name] => Woodstock 2021
-        [date] => 2020-07-10
-        [qr] => Endroid\QrCode\QrCode Object
-    
--->
-
 <p>&iexcl;Hola, <?= $data['name'] ?>!</p>
-<p>Detalles de compra:</p>
-<pre><big>Código de compra: <b><?= $data['id_purchase'] ?></b></big></pre>
-<br>
-<p>---------------------</p>
-
+<p>Los detalles de tu compra recientemente efectuada se encuentran aqu&iacute;.</p>
+<pre><big>C&oacute;digo de compra: <b><?= $data['id_purchase'] ?></b></big></pre>
+<pre><big>Fecha y hora de compra: <b><?= $data['purchase_date'] ?></b></big></pre>
+<p>-----------------------------</p>
 <?php foreach ($data['items'] as $key => $item): ?>
     <p>Item <?= $key + 1 ?></p>
-    <p>---------------------</p>
+    <p>-----------------------------</p>
+
     <pre><big>Evento: <?= $item['event_name'] ?></big></pre>
+
     <pre><big>Fecha: <?= $item['date'] ?></big></pre>
+
     <pre><big>Tipo asiento: <?= $item['seat_type'] ?></big></pre>
+
     <pre><big>Cantidad: <?= $item['amount'] ?></big></pre>
+
     <pre><big>Precio x unidad: <?= '$' . number_format((float)$item['price'], 2, ',', '.') ?></big></pre>
+
     <pre><big>Precio x cantidad: <?= $item['amount'] ?> x <?= '$' . number_format((float)$item['price'], 2, ',', '.') ?> = <?= '$' . number_format((float)$item['subtotal'], 2, ',', '.') ?></big></pre>
-    <pre><big>Código QR:</big></pre>
-    <?php 
 
-     ?>
-     <!-- PARA PRUEBAS EN EL NAVEGADOR -->
-     <img src="<?php echo $item['qr']->writeDataUri() ?>" alt="qrcode">
+    <pre><big>C&oacute;digo QR:</big></pre>
 
-     <!-- PARA E-MAIL -->
-     <?php /* <img src="cid:<?= $qr['cid'] ?>"> */ ?> 
-    <p>---------------------</p>
-    
+     <img src="cid:<?= $data['qr_cid'][$key] ?>"> 
+    <p>-----------------------------</p>
 <?php endforeach ?>
 
 
 <pre><big>Total compra: <?= '$' . number_format((float)$data['total'], 2, ',', '.') ?></big></pre>
-<br>
+<p>-----------------------------</p>
+<p>Para tu conveniencia, los c&oacute;digos qr correspondientes a tus tickets tambi&eacute;n se encuentran adjuntos en este correo.</p>
+<p>Muchas gracias por utilizar nuestros servicios.</p>
 <p>Visit&aacute; nuestra p&aacute;gina <a href="<?= FRONT_ROOT ?>">aqu&iacute;</a>.</p>
--- app/views/mail/purchase_details_plain.php
+++ app/views/mail/purchase_details_plain.php
@@ -1,8 +1,39 @@
 Hola, <?= $data['name'] ?>!
 
-Este es un mensaje de confirmación de compra efectuada. Gracias por utilizar nuestro servicio.
+Los detalles de tu compra recientemente efectuada se encuentran aquí.
 
-Total compra: <?= $data['total'] ?>
+Código de compra: <?= $data['id_purchase'] ?>
+
+Fecha y hora de compra: <?= $data['purchase_date'] ?>
+
+---------------------
+<?php foreach ($data['items'] as $key => $item): ?>
+Item <?= $key + 1 ?>
+
+---------------------
+
+Evento: <?= $item['event_name'] ?>
+
+Fecha: <?= $item['date'] ?>
+
+Tipo asiento: <?= $item['seat_type'] ?>
+
+Cantidad: <?= $item['amount'] ?>
+
+Precio x unidad: <?= '$' . number_format((float)$item['price'], 2, ',', '.') ?>
+
+Precio x cantidad: <?= $item['amount'] ?> x <?= '$' . number_format((float)$item['price'], 2, ',', '.') ?> = <?= '$' . number_format((float)$item['subtotal'], 2, ',', '.') ?>
+
+
+---------------------
+<?php endforeach ?>
+Total compra: <?= '$' . number_format((float)$data['total'], 2, ',', '.') ?>
+
+---------------------
+
+Los códigos qr correspondientes a tus tickets adquiridos se encuentran adjuntos en este correo.
+
+Muchas gracias por utilizar nuestros servicios.
 
 Visitá nuestra página aquí:
 
