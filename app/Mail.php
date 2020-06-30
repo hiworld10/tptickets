f40aae853dd19307640bd51ed4c0b4444fd416e8
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-06-30 16:41:51 -0300

    Overhaul of Mail functions. Now it properly sends an email with the purchase details to user.

--- app/Mail.php
+++ app/Mail.php
@@ -2,139 +2,123 @@
 
 namespace app;
 
-use core\Controller;
-use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
+use app\utils\StringUtils;
+use core\Controller;
 
 class Mail
 {
     private $mail;
 
     // Preparación inicial del mail
-    public function prepare($to, $subject)
+    private function prepare($to, $subject)
     {
         $this->mail = new PHPMailer(true);
 
-        try {
-            // $this->mail->SMTPDebug = 4;
+        $this->mail->SMTPDebug = 1;
 
-            // Configuración necesaria para utilizar un servidor SMTP local.
-            // Esto NO debe estar al usar un SMTP real.
-            $this->mail->SMTPOptions = [
-                'ssl' => [
-                    'verify_peer'       => false,
-                    'verify_peer_name'  => false,
-                    'allow_self_signed' => true,
-                ],
-            ];
-            
-            // Configuración de servidor
-            $this->mail->isSMTP();
-            $this->mail->Host = SMTP_HOST;
-            // $this->mail->SMTPAuth   = true;
-            // $this->mail->Username   = SMTP_USER;
-            // $this->mail->Password   = SMTP_PASS;
-            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $this->mail->Port       = SMTP_PORT;
+        // Configuración necesaria para utilizar un servidor SMTP local.
+        // Esto NO debe estar al usar un SMTP real.
+        $this->mail->SMTPOptions = [
+            'ssl' => [
+                'verify_peer'       => false,
+                'verify_peer_name'  => false,
+                'allow_self_signed' => true,
+            ],
+        ];
 
-            // Remitente y destinatario
-            $this->mail->setFrom(LOCAL_MAIL_DOMAIN, 'Monito Inc.');
-            $this->mail->addAddress($to);
+        // Configuración de servidor
+        $this->mail->isSMTP();
+        $this->mail->Host = SMTP_HOST;
+        // $this->mail->SMTPAuth   = true;
+        // $this->mail->Username   = SMTP_USER;
+        // $this->mail->Password   = SMTP_PASS;
+        $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $this->mail->Port       = SMTP_PORT;
 
-            // Conteido
-            $this->mail->isHTML(true);
-            $this->mail->Subject = $subject;
+        // Remitente y destinatario
+        $this->mail->setFrom(LOCAL_MAIL_DOMAIN, 'Monito Inc.');
+        $this->mail->addAddress($to); //Nombre opcional
 
-            return $this->mail;
-
-        } catch (Exception $e) {
-            throw new Exception('Error al preparar mail.');
-        }
+        // Conteido
+        $this->mail->isHTML(true);
+        $this->mail->Subject = $subject;
 
     }
 
-    public function send($to, $subject, $html, $text, $attachments = [])
+    private function addBody($html, $text)
     {
-        $this->mail = new PHPMailer(true);
+        $this->mail->Body    = $html;
+        $this->mail->AltBody = $text;
+    }
 
+    private function addAttachment($attachment)
+    {
+        $this->mail->addAttachment($attachment); //new file name is optional
+    }
+
+    private function addStringEmbeddedImage($embedded_image, $cid, $alt_name)
+    {
+        $this->mail->addStringEmbeddedImage($embedded_image, $cid, $alt_name);
+    }
+
+    private function send()
+    {
         try {
-            //$this->mail->SMTPDebug = 4;
-
-            $this->mail->SMTPOptions = [
-                'ssl' => [
-                    'verify_peer'       => false,
-                    'verify_peer_name'  => false,
-                    'allow_self_signed' => true,
-                ],
-            ];
-
-            //Server settings
-            $this->mail->isSMTP();
-            $this->mail->Host = SMTP_HOST;
-            // $this->mail->SMTPAuth   = true;
-            // $this->mail->Username   = SMTP_USER;
-            // $this->mail->Password   = SMTP_PASS;
-            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-            $this->mail->Port       = SMTP_PORT;
-
-            //Recipients
-            $this->mail->setFrom(LOCAL_MAIL_DOMAIN, 'Monito Inc.');
-            $this->mail->addAddress($to); // Name is optional
-
-            // Attachments
-            if (!empty($attachments)) {
-                foreach ($attachments as $value) {
-                    // Add attachments, new file name optional
-                    $this->mail->addAttachment($value['path']);
-                }
-            }
-
-            // Content
-            $this->mail->isHTML(true); // Set email format to HTML
-            $this->mail->Subject = $subject;
-            $this->mail->Body    = $html;
-            $this->mail->AltBody = $text;
-
             $this->mail->send();
-
-            return true;
         } catch (Exception $e) {
-            return false;
+            throw new Exception('Message could not be sent. Mailer Error: {$mail->ErrorInfo}');
         }
     }
 
     public function sendWelcomeMessage($to, $data)
     {
-        $this->send(
-            $to,
-            'Bienvenido a TPTickets',
+        $this->prepare($to, 'Bienvenido a TPTickets');
+        $this->addBody(
             Controller::getRenderedTemplate('mail/welcome_message_html', $data),
             Controller::getRenderedTemplate('mail/welcome_message_plain', $data)
         );
+        $this->send();
     }
 
     public function purchaseDetails($to, $data)
     {
-        $qr_data_for_embed = [];
-        $i                 = 1;
+        $this->prepare($to, 'Detalles de compra');
 
+        $i = 1;
+
+        // Almacenamiento en string de códigos qr, posteriormente agregados al mail
         foreach ($data['items'] as $value) {
-            $qr_data_for_embed[] = [
-                'code' => file_get_contents($value['qr']->writeDataUri()),
-                'cid'  => 'qr_' . $i,
-            ];
+            $qr_string = file_get_contents($value['qr']->writeDataUri());
+            $cid       = 'qr_' . $i;
+
+            $data['qr_cid'][] = $cid;
+
+            $this->addStringEmbeddedImage($qr_string, $cid, 'qrcode.png');
+            
+            $qr_file_path = 
+                PROJECT_ROOT . 
+                '/files/qr/' . 
+                $i . 
+                '_' .
+                StringUtils::lowercaseAndUnderscores(
+                    $value['event_name'] . '_'  . $value['seat_type']
+                ) .
+                '.png'
+            ;
+
+            $value['qr']->writeFile($qr_file_path);
+
+            $this->addAttachment($qr_file_path);
+
             $i++;
         }
 
-        echo '<pre>';
-        print_r($qr_data_for_embed);
-        echo '</pre>';
+        $this->addBody(
+            Controller::getRenderedTemplate('mail/purchase_details_html', $data),
+            Controller::getRenderedTemplate('mail/purchase_details_plain', $data)
+        );
 
-        // return::send(
-        //     $to,
-        //     'Detalles de tu compra',
-        //     Controller::getRenderedTemplate('mail/purchase_details_html', $data),
-        //     Controller::getRenderedTemplate('mail/purchase_details_plain', $data)
-        // );
+        $this->send();
     }
 }
