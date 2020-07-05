b95adf29db037d0bd51d5fe0db3e66e5bab46568
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-05 20:14:02 -0300

    Major changes in appearance and behavior in navbar
    Include website icon next to website name and switch for an 'admin area' title if admin is in session. Change color of buttons. Is fixed to the top.

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,18 +1,26 @@
-<nav class="navbar navbar-light bg-light static-top">
+<nav class="navbar navbar-dark bg-dark fixed-top">
     <div class="container">
-        <a class="navbar-brand" href="<?= FRONT_ROOT ?>/">TPTickets</a>
+        
+        <a class="navbar-brand d-flex" href="<?= FRONT_ROOT ?>/">
+            <?php if (\app\Auth::isAdmin()): ?>
+                <div class="pl-2 pt-1">TPTickets <small>Admin Area</small></div>
+            <?php else: ?>
+                <div><img src="<?= FRONT_ROOT ?>/tickets-blue.png" style="height: 34px;" alt=""></div>
+                <div class="pl-2">TPTickets</div>
+            <?php endif ?>
+        </a>
         <div class="right">
             <?php if(!isset($_SESSION['tptickets_user_id'])) : ?>
-                <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/login">Ingresar</a>
-                <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/register">Registrarse</a>
+                <a class="btn btn-info" href="<?= FRONT_ROOT ?>/users/login">Ingresar</a>
+                <a class="btn btn-info" href="<?= FRONT_ROOT ?>/users/register">Registrarse</a>
             <?php else :  ?>
                 <?php if (isset($_SESSION['tptickets_items'])): ?>
                     <a href="<?= FRONT_ROOT ?>/purchases/show-cart">Tu Compra: <?php echo htmlspecialchars('$' . number_format((float)$_SESSION['tptickets_subtotal'], 2, ',', '')) ?></a>
                 <?php endif ?>
                 <?php if (!\app\Auth::isAdmin()): ?>
-                    <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/show-profile">Mi cuenta</a>
+                    <a class="btn btn-info" href="<?= FRONT_ROOT ?>/users/show-profile">Mi cuenta</a>
                 <?php endif ?>
-                <a class="btn btn-primary" href="<?= FRONT_ROOT ?>/users/logout">Salir</a>
+                <a class="btn btn-info" href="<?= FRONT_ROOT ?>/users/logout">Salir</a>
             <?php endif; ?>  
         </div>
     </div>
