004bad216148e56c483a5f07e49ad3da9a758f63
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-13 16:51:21 -0300

    Remove unused functions in admin controllers

--- app/controllers/admin/EventSeats.php
+++ app/controllers/admin/EventSeats.php
@@ -2,8 +2,6 @@
 
 namespace app\controllers\admin;
 
-use app\models\EventSeat;
-
 class EventSeats extends \app\controllers\Authentication
 {
 
@@ -20,54 +18,5 @@ class EventSeats extends \app\controllers\Authentication
         $data['event_seats'] = $this->event_seat_dao->retrieveAll();
         //$seatTypeArray = $this->seat_type_dao->retrieveAll();
         $this->view('admin/event_seats', $data);
-    }
-
-    public function addEventSeat($calendarId, $seatType, $availableSeats, $price)
-    {
-        $remainder = $availableSeats;
-        $eventSeat = new EventSeat(null, $calendarId, $seatType, $availableSeats, $price, $remainder);
-        $this->dao->create($eventSeat);
-    }
-
-    public function addEventSeatAndView($calendarId, $seatType, $availableSeats, $price)
-    {
-        $remainder = $availableSeats;
-        $eventSeat = new EventSeat(null, $calendarId, $seatType, $availableSeats, $price, $remainder);
-        $this->dao->create($eventSeat);
-        $this->getAll();
-    }
-
-    public function getEventSeat($id)
-    {
-        $eventSeat     = $this->dao->retrieveById($id);
-        $seatTypeArray = $this->seatTypeController->getAll();
-        if (isset($eventSeat) && isset($calendarArray) && isset($seatTypeArray)) {
-            require ADMIN_VIEWS . '/admineventseat.php';
-        }
-    }
-
-    public function getAllSelect()
-    {
-        return $this->dao->retrieveAll();
-    }
-
-    public function deleteEventSeat($id)
-    {
-        $this->dao->delete($id);
-        $this->getAll();
-    }
-
-    public function updateEventSeat($id, $calendarId, $seatType, $availableSeats, $price)
-    {
-        $remainder = $availableSeats;
-
-        $updatedEventSeat = new EventSeat($id, $calendarId, $seatType, $availableSeats, $price, $remainder);
-
-        $this->dao->update($updatedEventSeat);
-    }
-
-    public function getByCalendarId($idCalendar)
-    {
-        return $this->dao->retrieveByCalendarId($idCalendar);
     }
 }
--- app/controllers/admin/PlaceEvents.php
+++ app/controllers/admin/PlaceEvents.php
@@ -2,8 +2,6 @@
 
 namespace app\controllers\admin;
 
-use app\models\PlaceEvent;
-
 class PlaceEvents extends \app\controllers\Authentication
 {
     private $dao;
@@ -19,53 +17,5 @@ class PlaceEvents extends \app\controllers\Authentication
     {
         $data['place_events'] = $this->dao->retrieveAll();
         $this->view('admin/place_events', $data);
-    }
-
-    public function addPlaceEvent($calendarId, $capacity, $description)
-    {
-        $placeEvent = new PlaceEvent(null, $calendarId, $capacity, $description);
-        $this->dao->create($placeEvent);
-    }
-
-    public function addPlaceEventAndView($calendarId, $capacity, $description)
-    {
-        $placeEvent = new PlaceEvent(null, $calendarId, $capacity, $description);
-        $this->dao->create($placeEvent);
-        $this->getAll();
-    }
-
-    public function getAllSelect()
-    {
-        return $this->dao->retrieveAll();
-    }
-
-    public function deletePlaceEvent($id)
-    {
-        $this->dao->delete($id);
-        $this->getAll();
-    }
-
-    public function getPlaceEvent($id)
-    {
-        $placeEvent = $this->dao->retrieveById($id);
-        if (isset($placeEvent)) {
-            require ADMIN_VIEWS . '/adminplaceevent.php';
-        }
-    }
-
-    public function getPlaceEventById($id)
-    {
-        return $this->dao->retrieveById($id);
-    }
-
-    public function getPlaceEventSelect($id)
-    {
-        return $this->dao->retrieveById($id);
-    }
-
-    public function updatePlaceEvent($id, $calendarId, $capacity, $description)
-    {
-        $updatedPlaceEvent = new PlaceEvent($id, $calendarId, $capacity, $description);
-        $this->dao->update($updatedPlaceEvent);
     }
 }
