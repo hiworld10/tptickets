872044f58a40e65a66dc0b41b333cbf3f72281e2
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-22 18:15:21 -0300

    Change seat list view style

--- app/views/calendars/list_seats.php
+++ app/views/calendars/list_seats.php
@@ -1,26 +1,44 @@
 <?php require HEADER ?>
 
-<div>
-    <h1><?php echo htmlspecialchars($data['event_name']) ?></h1>
-    <br>
-    <?php echo "Fecha de evento: " . htmlspecialchars($data['date']) ?>
+<div class="container-fluid" style="background-color:#F8F9FA;">
+    <h1 class="text-center mb-0"><?php echo htmlspecialchars($data['event_name']) ?></h1>
+    <p class="text-center mt-1">
+        <big><?php echo "Fecha de evento: " . htmlspecialchars($data['date']) ?></big>
+    </p>
+    <hr>
 </div>
 
-<div class="jumbotron">
-    <?php if (!empty($data['event_seats'])): ?>
-        <?php foreach ($data['event_seats'] as $event_seat): ?>
-            <div>
-                <pre><?php echo htmlspecialchars($event_seat->getSeatType()->getType()) ?></pre>
-                <pre><?php echo "Precio: $" . htmlspecialchars($event_seat->getPrice()) ?></pre>
-                <form name="form" action="<?= FRONT_ROOT ?>/calendars/show-seat" method="POST">
-                    <input type="hidden" name="id_event_seat" value="<?= $event_seat->getId() ?>">
-                    <button type="submit">Comprar</button>
-                </form>
-            </div>
-        <?php endforeach ?>      
-    <?php else: ?>
-        <pre>Entradas agotadas</pre>      
-    <?php endif ?>
+<div class="container">
+    <h3 class="text-center">Seleccioná tu asiento:</h3>
+    <hr>     
+
+    <div class="row">
+    
+        <?php if (!empty($data['event_seats'])): ?>
+            <?php foreach ($data['event_seats'] as $event_seat): ?>
+
+                <div class="col-lg-4">
+                    <div class="card mb-3" style="width: 18rem;">
+                        
+                        <div class="card-body">
+                            <h3 class="card-title text-center"><?php echo htmlspecialchars($event_seat->getSeatType()->getType()) ?></h3>
+                            <hr>
+                            <p class="card-text text-center"><?php echo "Precio: $" . htmlspecialchars($event_seat->getPrice()) ?></p>
+                            <a href="#" class="text-center">
+                                <form name="form" action="<?= FRONT_ROOT ?>/calendars/show-seat" method="POST">
+                                    <input type="hidden" name="id_event_seat" value="<?= $event_seat->getId() ?>">
+                                    <button type="submit" class="btn btn-info text-center">Comprar</button>
+                                </form>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+            <?php endforeach ?>
+        <?php else: ?>
+            <pre>Entradas agotadas</pre>      
+        <?php endif ?>          
+    </div>
 </div>
 
 <?php require FOOTER ?>
