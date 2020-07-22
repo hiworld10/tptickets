20e1acb5177221a30ea57a7a49bc1053a6ef8a0c
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-22 18:02:26 -0300

    Change footer text style

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-6 h-100 text-center text-lg-left my-auto">
-                <p class="text-dark small mb-4 mb-lg-0">&copy; Monito Inc. 2018. All Rights Reserved.</p>
+                <p class="text-muted small mb-4 mb-lg-0">&copy; Monito Inc. 2018. All Rights Reserved.</p>
             </div>
         </div>
     </div>
