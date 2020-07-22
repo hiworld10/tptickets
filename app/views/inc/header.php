f6359c1afaa4a812fa52b1cefc46b960cc2faab8
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-22 18:07:24 -0300

    Move custom css to its own file

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,26 +6,7 @@
     <link rel="stylesheet" href="<?= FRONT_ROOT ?>/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?= FRONT_ROOT ?>/css/admin.css">
     <link rel="icon" href="<?= FRONT_ROOT ?>/tickets-blue.png">
-    <style type="text/css">
-        main {
-            background-image: url("<?= IMG_FRONT_ROOT ?>/fondologin.jpg");
-            height: auto;
-            width: auto;
-        }
-        h2 {
-            color: white;
-        }
-        body { 
-            background-color: #fafafa;
-            padding-top: 60px; 
-        }
-        a {
-            color: #00BBD3;
-        }
-        a:hover {
-            color: #00BBD3;
-        }
-    </style>
+    <link rel="stylesheet" href="<?= FRONT_ROOT ?>/css/custom.css">
 </head>
 
 <?php require NAVBAR ?>
