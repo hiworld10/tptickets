7edaba570f746e04909838b2ee693440b8ee223a
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-22 18:02:00 -0300

    Change home page style

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,29 +1,34 @@
-<?php require HEADER; ?>
+<?php require HEADER ?>
 
-<header class="masthead text-black text-center">
-    <div class="overlay"></div>
+<div class="bg-dark" style="background-image: url(<?=  FRONT_ROOT ."/img/imghome.jpg"?>); width: 100%; height: 750px">
     <div class="container">
-    <div class="row">
-        <div class="col-xl-9 mx-auto">
-            <h1 class="mb-5">La forma mas facil de comprar tus tickets a un solo click!</h1>
-        </div>
-        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-            <form action="<?=  FRONT_ROOT ."/home/search"?>"  method="GET">
-                <div class="form-row">
-                    <div class="col-12 col-md-9 mb-2 mb-md-0">
-                        <input type="text" name="q" class="form-control form-control-lg" placeholder="Buscar por evento o artista..." autofocus>
-                    </div>
-                    <div class="col-12 col-md-3">
-                        <button type="submit" class="btn btn-block btn-lg btn-info">Buscar</button>
-                    </div>
-                </div>
-            </form>
-        </div> 
-    </div>
-</header>
+        <div class="row">
+            <div class="col-12">
+                <div class="text-center align-content-center">
+                    <br>
+                    <br>
+                    <h1 class="mb-5 alert bg-dark text-light">
+                        ¡La forma mas fácil de comprar tus tickets a un solo click!
+                    </h1>
 
-<div>
-    <table class="table bg-light-alpha text-center mt-5">
+                        <form action="<?=  FRONT_ROOT ."/home/search"?>"  method="GET">
+                            <div class="form-row">
+                                <div class="col-12 col-md-9 mb-2 mb-md-0">
+                                    <input type="text" name="q" class="form-control form-control-lg" placeholder="Buscar por evento o artista..." autofocus>
+                                </div>
+                                <div class="col-12 col-md-3">
+                                    <button type="submit" class="btn btn-block btn-lg btn-info">Buscar</button>
+                                </div>
+                            </div>
+                        </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <table class="table bg-light-alpha text-center mt-5 mb-3">
         <?php if(!empty($data['events'])): ?>
             <tbody>
             <?php foreach ($data['events'] as $value): ?>
@@ -48,4 +53,4 @@
     </table>
 </div>
 
-<?php require FOOTER; ?>
+<?php require FOOTER ?>
