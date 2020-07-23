5c2670e20ea0782821e40dd4545922c1345de861
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-23 19:31:42 -0300

    Create admin Bundles controller, add index method and view

---
+++ app/controllers/admin/Bundles.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\controllers\admin;
+
+use core\View;
+
+class Bundles extends \app\controllers\Authentication
+{
+    public function __construct()
+    {
+        // $this->bundle_dao = $this->dao('bundle');
+    }
+
+    public function index()
+    {
+        // $data['bundles'] = $this->bundle_dao->retrieveAll();
+        View::render('admin/bundles', /*$data*/);
+    }
+}
---
+++ app/views/admin/bundles.php
@@ -0,0 +1,11 @@
+<?php require HEADER ?>
+
+<div id="container" class="__full-height-perc">
+    <?php require ADMIN_NAVBAR ?>
+
+    <div id="divform" class="__full-height-perc">
+        <p>Paquetes</p>    
+    </div>
+</div>
+
+<?php require FOOTER ?>
