b52d6d3828b71c3ec9cf46cedf77183133e795d2
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-24 00:59:16 -0300

    All crud functions for Bundle model implemented, can work via admin

--- app/controllers/admin/Bundles.php
+++ app/controllers/admin/Bundles.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers\admin;
 
+use app\utils\Flash;
 use core\View;
 
 class Bundles extends \app\controllers\Authentication
@@ -15,5 +16,48 @@ class Bundles extends \app\controllers\Authentication
     {
         $data['bundles'] = $this->bundle_dao->retrieveAll();
         View::render('admin/bundles', $data);
+    }
+
+    public function add()
+    {
+        $this->redirectIfRequestIsNotPost('/admin/bundles');
+
+        $data = [
+            'description' => trim($_POST['description']),
+            'discount'    => trim($_POST['discount']),
+        ];
+        $this->bundle_dao->create($data);
+
+        Flash::addMessage('Paquete agregado.');
+        $this->redirect('/admin/bundles');
+    }
+
+    public function edit($id)
+    {
+        $data['bundle'] = $this->bundle_dao->retrieveById($id);
+        if (isset($data['bundle'])) {
+            View::render('admin/bundles', $data);
+        }
+    }
+
+    public function update($id)
+    {
+        $this->redirectIfRequestIsNotPost('/admin/bundles');
+
+        $data['id_bundle']   = $id;
+        $data['description'] = $_POST['description'];
+        $data['discount']    = $_POST['discount'];
+        $this->bundle_dao->update($data);
+
+        Flash::addMessage('Paquete actualizado.');
+        $this->redirect('/admin/bundles');
+    }
+
+    public function delete($id)
+    {
+        //Se tendría que agregar una confirmación adicional para eliminar el item, para evitar el borrado accidental.
+        $this->redirectIfRequestIsNotPost('/admin/bundles');
+        $this->handleDeleteCascadeConstraint($this->bundle_dao, $id);
+        $this->redirect('/admin/bundles');
     }
 }
--- app/views/admin/bundles.php
+++ app/views/admin/bundles.php
@@ -4,12 +4,51 @@
     <?php require ADMIN_NAVBAR ?>
 
     <div id="divform" class="__full-height-perc">
+    
+        <?php if(isset($data['bundle'])): ?>
 
+            <form name='formulario' action="<?=FRONT_ROOT?>/admin/bundles/update"  enctype="multipart/form-data" method="POST">
+                <div class="form-row p-2 mt-4">
+                    <input type="hidden" name="id_bundle" value="<?= $data['bundle']->getId() ?>">
+                    <div class="col-lg-8 form-group mt-4">
+                        <label for="">Paquete</label>
+                        <input type="text"  class="form-control form-control-lg" name="description" value="<?= $data['bundle']->getDescription() ?>"placeholder="Ingrese nombre del paquete..." required>
+                    </div>
+                    <div class="col-lg-2 form-group mt-4">
+                        <label for="">Descuento (%)</label>
+                        <input type="number" min="5" max="50" class="form-control form-control-lg" name="discount" value="<?= $data['bundle']->getDiscount() ?>" required>
+                    </div>
+                    <div class="col-11 col-md-3 mt-4">
+                        <button type="submit" class="btn btn-block btn-lg btn-info">Agregar</button>
+                    </div>
+                </div>
+            </form>
+
+        <?php else: ?>
+
+            <form name='formulario' action="<?=FRONT_ROOT?>/admin/bundles/add"  enctype="multipart/form-data" method="POST">
+                <div class="form-row p-2 mt-4">
+                    <div class="col-lg-8 form-group mt-4">
+                        <label for="">Paquete</label>
+                        <input type="text"  class="form-control form-control-lg" name="description" placeholder="Ingrese nombre del paquete..." required>
+                    </div>
+                    <div class="col-lg-2 form-group mt-4">
+                        <label for="">Descuento (%)</label>
+                        <input type="number" min="5" max="50" class="form-control form-control-lg" name="discount" required>
+                    </div>
+                    <div class="col-11 col-md-3 mt-4">
+                        <button type="submit" class="btn btn-block btn-lg btn-info">Agregar</button>
+                    </div>
+                </div>
+            </form>
+
+        <?php endif ?>
+        
         <!-- Lista de paquetes -->
         <div id="table">
             <table class="table bg-light-alpha">
 
-                <?php if (isset($data['bundles'])): ?>
+                <?php if (!empty($data['bundles'])): ?>
                     <thead>     
                         <th>Id</th>  
                         <th>Descripción</th>        
