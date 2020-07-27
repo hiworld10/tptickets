c15640357258a191af5b15dffc0f08b4088c3ec4
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-26 22:54:04 -0300

    Add container div to purchase success view

--- app/views/purchases/success.php
+++ app/views/purchases/success.php
@@ -1,9 +1,10 @@
 <?php require HEADER ?>
 
-<h1>&iexcl;Éxito!</h1><br>
+<div class="container">
+    <h1>&iexcl;Éxito!</h1><br>
 
-<p>Tu compra se ha efectuado exitosamente. Te hemos enviado un e-mail con los detalles de tu compra.</p>
-<p>Gracias por utilizar nuestro servicio.</p>
+    <p>Tu compra se ha efectuado exitosamente. Te hemos enviado un e-mail con los detalles de tu compra.</p>
+    <p>Gracias por utilizar nuestro servicio.</p>
 <a href="<?= FRONT_ROOT ?>">Volver a la página principal</a>
-
+</div>
 <?php require FOOTER ?>
