a92be2212217285555fbe399c2d6d0bf1e01c34c
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-07-30 19:52:20 -0300

    Align dl entries in show_profile view, add purchases history link (wip)

--- app/views/users/show_profile.php
+++ app/views/users/show_profile.php
@@ -2,14 +2,18 @@
 
 <div class="container">
     <h1>Tu Perfil</h1><br>
-    <dl class="dl-horizontal">
-        <dt>Nombre</dt>
-        <dd><?php echo htmlspecialchars($data['user']->getName()) ?></dd>
-        <dt>Apellido</dt>
-        <dd><?php echo htmlspecialchars($data['user']->getSurname()) ?></dd>
-        <dt>E-mail</dt>
-        <dd><?php echo htmlspecialchars($data['user']->getEmail()) ?></dd>
+    <h3>Información personal</h3><br>
+    <dl class="row">
+        <dt class="col-sm-3">Nombre</dt>
+        <dd class="col-sm-9"><?php echo htmlspecialchars($data['user']->getName()) ?></dd>
+        <dt class="col-sm-3">Apellido</dt>
+        <dd class="col-sm-9"><?php echo htmlspecialchars($data['user']->getSurname()) ?></dd>
+        <dt class="col-sm-3">E-mail</dt>
+        <dd class="col-sm-9"><?php echo htmlspecialchars($data['user']->getEmail()) ?></dd>
     </dl>
     <a href="<?= FRONT_ROOT ?>/users/edit-profile">Editar perfil</a>
+    <br><br>
+    <h3>Historial de compras</h3>
+    <a href="#">Ver historial</a>
 </div>
 <?php require FOOTER ?>
