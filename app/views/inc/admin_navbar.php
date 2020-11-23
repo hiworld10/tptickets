1de22050477f23c558c5fa4bee697a7392985bcb
hiworld10 <58235535+hiworld10@users.noreply.github.com>
2020-11-23 19:23:31 -0300

    Reorder admin navbar elements

--- app/views/inc/admin_navbar.php
+++ app/views/inc/admin_navbar.php
@@ -4,11 +4,11 @@
         <li><a href="<?=FRONT_ROOT?>/admin/calendars">Calendarios</a></li>
         <li><a href="<?=FRONT_ROOT?>/admin/categories" >Categorias</a></li>
         <li><a href="<?=FRONT_ROOT?>/admin/events">Eventos</a></li>
+        <li><a href="<?=FRONT_ROOT?>/admin/bundles">Paquetes</a></li>
         <li><a href="<?=FRONT_ROOT?>/admin/seat-types">Tipo Plazas</a></li>
         <li><a href="<?=FRONT_ROOT?>/admin/purchases">Compras</a></li>
         <li><a href="<?=FRONT_ROOT?>/admin/purchase-lines">Lineas Compra</a></li>
         <li><a href="<?=FRONT_ROOT?>/admin/tickets">Tickets</a></li>
-        <li><a href="<?=FRONT_ROOT?>/admin/bundles">Paquetes</a></li>
         <li><a href="<?=FRONT_ROOT?>/admin/sales">Ventas</a></li>
         <li><a href="<?=FRONT_ROOT?>/users">Usuarios</a></li>
     </ul>
